1ba3169d53bbd683a9cf69b4c4de5bcc2ca4040e
wjia <wjia@windriver.com>
2016-05-13 13:10:18 -0400

    Edited the qos specs/volume type keywords function and updated the testcases

--- CGCSAuto/testcases/functional/storage/test_associate_qos_spec_to_volume.py
+++ CGCSAuto/testcases/functional/storage/test_associate_qos_spec_to_volume.py
@@ -16,7 +16,7 @@ from setup_consts import P1, P2, P3
 @fixture(scope='module')
 def create_volume_type(request):
 
-    volume_type_id = cinder_helper.create_volume_type("test_volume_type")[0]
+    volume_type_id = cinder_helper.create_volume_type("test_volume_type")[1]
 
     volume_type = {'id': volume_type_id
                    }
@@ -31,7 +31,7 @@ def create_volume_type(request):
 @fixture(scope='module')
 def create_qos_specs(request):
 
-    qos_specs_id = cinder_helper.create_qos_specs("test_qos_specs", a=123)[0]
+    qos_specs_id = cinder_helper.create_qos_specs("test_qos_specs", a=123)[1]
 
     qos_specs = {'id': qos_specs_id
                  }
@@ -66,32 +66,27 @@ def create_qos_association(request,create_volume_type, create_qos_specs):
 
 def test_verify_volume_type(create_volume_type):
     # use cinder type list find the newly create volume id exist
-    table_ = cinder_helper.get_type_list()
+    vol_type_id_list = cinder_helper.get_type_list()
+
     volume_type_id = create_volume_type['id']
 
-    match_vol_type_id = table_parser.get_values(table_, 'ID', ID=volume_type_id)[0]
-
-    assert volume_type_id == match_vol_type_id,"expected volume type ID to be {}. Actual ID is {} instead.".format(
-        volume_type_id, match_vol_type_id)
+    assert volume_type_id in vol_type_id_list, "expected volume type ID {} to be created. Actual ID is not in " \
+                                               "cinder type-list: {}.".format(volume_type_id, vol_type_id_list)
 
 
 def test_verify_qos_specs(create_qos_specs):
 
-    table_ = cinder_helper.get_qos_list()
+    qos_spec_id_list = cinder_helper.get_qos_list()
     qos_spec_id = create_qos_specs['id']
 
-    match_qos_spec_id = table_parser.get_values(table_, 'ID', ID=qos_spec_id)[0]
-
-    assert qos_spec_id == match_qos_spec_id, "expected QOS specs ID to be {}. Actual ID is {} instead.".format(
-        qos_spec_id, match_qos_spec_id)
+    assert qos_spec_id in qos_spec_id_list, "expected QOS specs ID to be {}. Actual ID is not in " \
+                                            "cinder qos-list: {}.".format(qos_spec_id, qos_spec_id_list)
 
 
 def test_associate_qos_spec_to_volume_type(create_qos_association):
 
     volume_type_id = create_qos_association['volume_type_id']
     qos_spec_id = create_qos_association['qos_id']
-
-    print(volume_type_id, qos_spec_id)
 
     table_ = cinder_helper.get_qos_association(qos_spec_id)
 
