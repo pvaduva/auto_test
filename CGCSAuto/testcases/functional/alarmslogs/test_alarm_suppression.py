6c55febe35972e6118887ba5a6ef45a4580e5d32
Anujeyan.Manokeran <Anujeyan.Manokeran@windriver.com>
2016-05-19 09:07:12 -0400

    Commiting test_alarm_suppression.py and test_logs.py

--- CGCSAuto/testcases/functional/alarmslogs/test_alarm_suppression.py
+++ CGCSAuto/testcases/functional/alarmslogs/test_alarm_suppression.py
@@ -4,21 +4,20 @@ from utils.tis_log import LOG
 from consts.cgcs import UUID
 from keywords import system_helper
 from utils import cli, table_parser
-# This test case is  to verify the query alarm and logs using event list.
-# Mainly it starts on with generating alarm alarm id 300.005 Critical major minot and not applicable and query them.
+# This test case is  to verify Alarm Suppression on Active alarm list
 
 
 def test_alarm_suppression():
     """
-       Test logs is to verify log generation of Critical Major Minor of alarms and logs
-       Args:
-           test_event_list event_option and Severity
+       Verify suppression and unsuppression of active alarm and query alarms.
 
        Test Setup:
            - Generate alarms according to severity
        Test Steps:
-           - Query alarms/logs with severity
-           - Verify generated alarm was success.
+            Suppress alarms
+            Verify alarm supressed
+            Unsuppressed alarm
+            Verify unsuppressed in active alarm list.
        Test Teardown:
            - None
     """
@@ -28,14 +27,14 @@ def test_alarm_suppression():
     con_ssh_act = ControllerClient.get_active_controller()
     alarm_log_generate_str = "fmClientCli -c  \"### ###" + alarmid + "###set###system.vm###host=compute-0.vm=$i### ###"\
                              "critical### ###processing-error###Automation Generate### ###True###True###\""
-    alarm_generate_succ = generate_alarm_log(con_ssh=con_ssh_act,alarm_str=alarm_log_generate_str,maxi=int(limit))
+    alarm_generate_succ = generate_alarm_log(con_ssh=con_ssh_act, alarm_str=alarm_log_generate_str, maxi=int(limit))
     assert alarm_generate_succ, "Alarm Generated"
-    query_active_alarm = system_helper.get_alarms(con_ssh=con_ssh_act,query_key='alarm_id',query_value=alarmid,
+    query_active_alarm = system_helper.get_alarms(con_ssh=con_ssh_act, query_key='alarm_id', query_value=alarmid,
                                                   query_type='string')
     assert len(query_active_alarm) > 1, "Alarm " + alarmid + " not found in active list  "
     LOG.tc_step('Alarm Suppressed .')
-    assert suppress_unsuppress_alarm(alarm_id=alarmid, con_ssh=con_ssh_act,suppress=True), "Alarm suppressed for " \
-                                                                                              "alarm ID " + alarmid
+    assert suppress_unsuppress_alarm(alarm_id=alarmid, con_ssh=con_ssh_act, suppress=True), "Alarm suppressed for " \
+                                                                                            "alarm ID " + alarmid
     query_active_alarm = system_helper.get_alarms(con_ssh=con_ssh_act, query_key='alarm_id', query_value=alarmid,
                                                   query_type='string')
     assert bool(query_active_alarm), "Alarm ID " + alarmid + "found in Active list"
@@ -55,7 +54,7 @@ def suppress_unsuppress_alarm(alarm_id=None, con_ssh=None, suppress=True):
     Returns:
         success/failure
     """
-    if not alarm_id :
+    if not alarm_id:
         return False
     query_alarm_suppress_list = system_helper.get_suppressed_alarms(uuid=True, con_ssh=con_ssh)
     if suppress:
@@ -76,8 +75,7 @@ def suppress_unsuppress_alarm(alarm_id=None, con_ssh=None, suppress=True):
         return True
 
 
-def generate_alarm_log(con_ssh = None , alarm_str='', maxi=0):
-    con_ssh = ControllerClient.get_active_controller()
+def generate_alarm_log(con_ssh=None, alarm_str='', maxi=0):
     for i in range(maxi):
         rtn_code, output = con_ssh.exec_cmd(cmd=alarm_str)
         # check UUID returned.
@@ -86,4 +84,3 @@ def generate_alarm_log(con_ssh = None , alarm_str='', maxi=0):
             return False
     else:
         return True
-
