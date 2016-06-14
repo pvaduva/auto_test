6495654d8dae92e4a4dc99aaa35dcc6b5d0adb9e
Nimalini Rasa <nimalini.rasa@windriver.com>
2016-06-14 22:25:14 +0000

    updated

--- CGCSAuto/testcases/functional/heat/test_heat.py
+++ CGCSAuto/testcases/functional/heat/test_heat.py
@@ -13,147 +13,161 @@ from consts.cgcs import HEAT_PATH
 import os
 from consts.auth import Tenant
 
-def get_heat_params(param_name=None):
-    if param_name is 'NETWORK':
-        net_id = network_helper.get_mgmt_net_id()
-        return network_helper.get_net_name_from_id(net_id=net_id)
-    elif param_name is 'FLAVOR':
-        return 'small'
-    elif param_name is 'IMAGE':
-        return 'cgcs-guest'
-    else:
-        return 1
 
+def verify_heat_resource(to_verify=None,template_name=None,stack_name=None,auth_info=None):
+    """
+        Verify the heat resource creation/deletion for given resources
 
-def verify_heat_resource(dic_to_verify=None):
-    for key in dic_to_verify:
-        if key is 'volume' :
-            #vol_name = dic_to_verify[key]
-            volume_id = cinder_helper.get_volumes(name='sample_volume')
-            if volume_id is None:
-                return 1
-        elif key is 'ceilometer_alarm':
-            LOG.info("Verifying ceilometer alarm creation via heat")
-            table = ceilometer_helper.get_alarms()
-            alarm_id = table_parser.get_values(table, 'Alarm ID', Name='STACK1', strict=False)
-            if alarm_id is None:
-                return 1
-        elif key is 'neutron_port':
-            LOG.info("Verifying neutron port creation via heat")
-            port_id = network_helper.get_neutron_port(name='sample_port')
-            if port_id is None:
-                return 1
-        elif key is 'neutron_provider_net':
-            LOG.info("Verifying neutron provider net creation via heat")
-            net_id = network_helper.get_provider_net(name='Sample_phynet')
-            if net_id is None:
-                return 1
-        elif key is 'neutron_provider_net_range':
-            LOG.info("Verifying neutron provider net range creation via heat")
-            net_range = network_helper.get_provider_net_range(name='Sample_phynet_X')
-            if net_range is None:
-                return 1
-        elif key is 'nova_server_group':
-            LOG.info("Verifying nova server group creation via heat")
-            server_group_id = nova_helper.get_server_groups(name='STACK1')
-            if server_group_id is None:
-                return 1
-        elif key is 'vm':
-            LOG.info("Verifying server creation via heat")
-            vm_id = nova_helper.get_vm_id_from_name(vm_name='nova_server')
-            if vm_id is None:
-                return 1
-        elif key is 'nova-flavor':
-            LOG.info("Verifying nova flavor creation via heat")
-            flavor_id = nova_helper.get_flavor_id(name='sample-flavor')
-            if flavor_id is None:
-                return 1
-        elif key is 'neutron_net':
-            LOG.info("Verifying neutron net creation via heat")
-            net_id = network_helper.get_tenant_net_id(net_name='sample-net')
-            if net_id is None:
-                return 1
-        elif key is 'image':
-            LOG.info("Verifying glance image creation via heat")
-            image_id = glance_helper.get_image_id_from_name(name='sample_image')
-            if image_id is None:
-                return 1
-        elif key is 'subnet':
-            LOG.info("Verifying subnet image creation via heat")
-            subnet_id = network_helper.get_subnets(name='sample_subnet')
-            if subnet_id is None:
-                return 1
-        elif key is 'floating_ip':
-            LOG.info("Verifying floating ip creation via heat")
-            floating_ip_id = network_helper.get_floating_ips()
-            if floating_ip_id is None:
-                return 1
-        elif key is 'router':
-            LOG.info("Verifying router creation via heat")
-            router_id = network_helper.get_tenant_router(router_name='sample_router')
-            if router_id is None:
-                return 1
-        elif key is 'router_gateway':
-            LOG.info("Verifying router gateway creation via heat")
-            router_id = network_helper.get_tenant_router(router_name='sample_gateway_router')
-            if router_id is None:
-                return 1
-            gateway_info = network_helper.get_router_ext_gateway_info(router_id=router_id)
-            if gateway_info is None:
-                return 1
-        elif key is 'router_interface':
-            LOG.info("Verifying router interface creation via heat")
-            router_id = network_helper.get_tenant_router(router_name='sample_if_router')
-            if router_id is None:
-                return 1
-            LOG.info("Verifying subnet creation via heat")
-            subnet_id = network_helper.get_subnets(name='sample_if_subnet')
-            if subnet_id is None:
-                return 1
-            router_subnets = network_helper.get_router_subnets(router_id=router_id)
-            if subnet_id not in router_subnets:
-                return 1
-        elif key is 'security_group':
-            LOG.info("Verifying neutron security group creation via heat")
-            security_group = network_helper.get_security_group(name='SecurityGroupDeluxe')
-            if security_group is None:
-                return 1
-        elif key is 'key_pair':
-            LOG.info("Verifying nova key pair creation via heat")
-            security_group = nova_helper.get_key_pair(name='KeyPairDeluxe')
-            if security_group is None:
-                return 1
-        elif key is 'neutron_qos':
-            LOG.info("Verifying neutron qos policy creation via heat")
-            qos_id = network_helper.get_qos(name='SampleQoS')
-            if qos_id is None:
-                return 1
-    return 0
+        Args:
+            to_verify (list): Resources to verify creation or deletion.
+            template_name (str): template to be used to create heat stack.
+            stack_name(str): stack name used to create the stack
 
-def verify_basic_template(template_name=None, template_path=None, con_ssh=None, auth_info=None):
+        Returns (int): return 0 if success 1 if failure
 
-    """Create a stack and then list stacks.
-        :param template_name: Name of the template
-        :param template_path: path to heat template
-        :auth_info: authentication info
+    """
+
+    if to_verify is 'volume' :
+        vol_name = getattr(Heat, template_name)['vol_name']
+        volume_id = cinder_helper.get_volumes(name=vol_name)
+        if volume_id:
+            return 0
+    elif to_verify is 'ceilometer_alarm':
+        LOG.info("Verifying ceilometer alarm creation via heat")
+        table = ceilometer_helper.get_alarms()
+        alarm_id = table_parser.get_values(table, 'Alarm ID', Name=stack_name, strict=False)
+        if alarm_id:
+            return 0
+    elif to_verify is 'neutron_port':
+        port_name = getattr(Heat, template_name)['port_name']
+        if port_name is None:
+            port_name = stack_name
+        LOG.info("Verifying neutron port creation via heat")
+        port_id = network_helper.get_neutron_port(name=port_name)
+        if port_id:
+            return 0
+    elif to_verify is 'neutron_provider_net':
+        LOG.info("Verifying neutron provider net creation via heat")
+        net_id = network_helper.get_provider_net(name='physnetX')
+        if net_id:
+            return 0
+    elif to_verify is 'neutron_provider_net_range':
+        LOG.info("Verifying neutron provider net range creation via heat")
+        net_range = network_helper.get_provider_net_range(name='sample_physnet_X')
+        if net_range:
+            return 0
+    elif to_verify is 'nova_server_group':
+        LOG.info("Verifying nova server group creation via heat")
+        server_group_id = nova_helper.get_server_groups(name=stack_name)
+        if server_group_id:
+            return 0
+    elif to_verify is 'vm':
+        vm_name = getattr(Heat, template_name)['vm_name']
+        LOG.info("Verifying server creation via heat")
+        vm_id = nova_helper.get_vm_id_from_name(vm_name=vm_name)
+        if vm_id:
+            return 0
+    elif to_verify is 'nova_flavor':
+        LOG.info("Verifying nova flavor creation via heat")
+        flavor_id = nova_helper.get_flavor_id(name='sample-flavor')
+        if flavor_id:
+            return 0
+    elif to_verify is 'neutron_net':
+        LOG.info("Verifying neutron net creation via heat")
+        net_id = network_helper.get_tenant_net_id(net_name='sample-net')
+        if net_id:
+            return 0
+    elif to_verify is 'image':
+        LOG.info("Verifying glance image creation via heat")
+        image_id = glance_helper.get_image_id_from_name(name='sample_image')
+        if image_id:
+            return 0
+    elif to_verify is 'subnet':
+        LOG.info("Verifying subnet image creation via heat")
+        subnet_id = network_helper.get_subnets(name='sample_subnet')
+        if subnet_id:
+            return 0
+    elif to_verify is 'floating_ip':
+        LOG.info("Verifying floating ip creation via heat")
+        floating_ip_id = network_helper.get_floating_ips()
+        if floating_ip_id:
+            return 0
+    elif to_verify is 'router':
+        LOG.info("Verifying router creation via heat")
+        router_id = network_helper.get_tenant_router(router_name='sample_router', auth_info=auth_info)
+        if router_id:
+            return 0
+    elif to_verify is 'router_gateway':
+        LOG.info("Verifying router gateway creation via heat")
+        router_id = network_helper.get_tenant_router(router_name='sample_gateway_router', auth_info=auth_info)
+        if not router_id:
+            return 1
+        gateway_info = network_helper.get_router_ext_gateway_info(router_id=router_id, auth_info=auth_info)
+        if gateway_info:
+            return 0
+    elif to_verify is 'router_interface':
+        LOG.info("Verifying router interface creation via heat")
+        router_id = network_helper.get_tenant_router(router_name='sample_if_router', auth_info=auth_info)
+        if not router_id:
+            return 1
+        LOG.info("Verifying subnet creation via heat")
+        subnet_id = network_helper.get_subnets(name='sample_if_subnet', auth_info=auth_info)
+        if not subnet_id:
+            return 1
+        router_subnets = network_helper.get_router_subnets(router_id=router_id, auth_info=auth_info)
+        if subnet_id in router_subnets:
+            return 0
+    elif to_verify is 'security_group':
+        LOG.info("Verifying neutron security group creation via heat")
+        security_group = network_helper.get_security_group(name='SecurityGroupDeluxe')
+        if security_group:
+            return 0
+    elif to_verify is 'key_pair':
+        kp_name = getattr(Heat, template_name)['key_pair_name']
+        LOG.info("Verifying nova key pair creation via heat")
+        key_pair_name = nova_helper.get_key_pair(name=kp_name)
+        if key_pair_name:
+            return 0
+    elif to_verify is 'neutron_qos':
+        LOG.info("Verifying neutron qos policy creation via heat")
+        qos_id = network_helper.get_qos(name='SampleQoS', auth_info=auth_info)
+        if qos_id:
+            return 0
+    return 1
+
+def verify_basic_template(template_name=None, con_ssh=None, auth_info=None):
+    """
+        Create/Delete heat stack and verify the resource are created/deleted as expeted
+            - Create a heat stack with the given template
+            - Verify heat stack is created sucessfully
+            - Verify heat resources are created
+            - Delete Heat stack and verify resource deletion
+        Args:
+            con_ssh (SSHClient): If None, active controller ssh will be used.
+            template_name (str): template to be used to create heat stack.
+            auth_info (dict): Tenant dict. If None, primary tenant will be used.
+
+        Returns (tuple): (rnt_code (int), message (str))
+
     """
 
     fail_ok=0
 
-    #param, param_val = create_param_list(template=template_name,ssh_client=con_ssh,auth_info=auth_info)
-    stack_name = 'STACK1'
     t_name, yaml = template_name.split('.')
     params = getattr(Heat, t_name)['params']
     heat_user = getattr(Heat, t_name)['heat_user']
+    stack_name = t_name
+    to_verify = getattr(Heat, t_name)['verify']
     if heat_user is 'admin':
         auth_info=Tenant.ADMIN
-    # heat_param.append('-P FLAVOR_NAME=%s' % flavor_name)
+
     template_path = os.path.join(HOME, HEAT_PATH, template_name)
     cmd_list = ['-f %s ' % template_path]
+
     if params is not None:
         for param in params:
-            param_result = get_heat_params(param_name=param)
-            cmd_list.append("-P %s=%s" % (param, param_result))
+            param_result = heat_helper.get_heat_params(param_name=param)
+            cmd_list.append("-P %s=%s " % (param, param_result))
 
     cmd_list.append(" %s" % stack_name)
     params_string = ''.join(cmd_list)
@@ -165,21 +179,19 @@ def verify_basic_template(template_name=None, template_path=None, con_ssh=None, 
         LOG.warning("Create heat stack request rejected.")
         return [1, output]
     LOG.info("Stack {} created sucessfully.".format(stack_name))
-    time.sleep(20)
+    time.sleep(50)
     LOG.tc_step("Verifying Heat Stack Status for CREATE_COMPLETE for stack %s",stack_name)
     stack_status = heat_helper.get_stack_status(stack_name=stack_name, auth_info=auth_info)
     if "CREATE_COMPLETE" not in stack_status:
         LOG.warning("Create heat stack Failed %s",stack_status)
         return [1, stack_status]
     LOG.info("Stack {} is in expected CREATE_COMPLETE state.".format(stack_name))
-
-    LOG.tc_step("Verifying Heat created resources for stack %s", stack_name)
-    to_verify = getattr(Heat, t_name)['verify']
-    verify_result = verify_heat_resource(to_verify)
-
-    if verify_result is not 0:
-        LOG.warning("Verify resouces created by heat stack Failed.")
-        return [1, output]
+    for item in to_verify:
+        LOG.tc_step("Verifying Heat created resources %s for stack %s", item, stack_name)
+        verify_result = verify_heat_resource(to_verify=item, template_name=t_name,stack_name=stack_name,auth_info=auth_info)
+        if verify_result is not 0:
+            LOG.warning("Verify resouces %s created by heat stack Failed.", item)
+            return [1, output]
 
     LOG.info("Stack {} resources are created expected.".format(stack_name))
     LOG.tc_step("Delete heat stack {} ".format(stack_name))
@@ -199,21 +211,25 @@ def verify_basic_template(template_name=None, template_path=None, con_ssh=None, 
 @mark.usefixtures('check_alarms')
 @mark.parametrize(
     ('template_name'), [
-        P1(('WR_Neutron_ProviderNetRange.yaml')),
-        P1(('WR_Neutron_ProviderNet.yaml')),
-        P1(('OS_Cinder_Volume.yaml')),
-        P1(('OS_Ceilometer_Alarm.yaml')),
-        P1(('OS_Neutron_Port.yaml')),
-        P1(('OS_Neutron_Net.yaml')),
-        P1(('OS_Neutron_Subnet.yaml')),
-        P1(('OS_Nova_Flavor.yaml')),
-        P1(('OS_Neutron_FloatingIP.yaml')),
-        P1(('OS_Neutron_Router.yaml')),
-        P1(('OS_Neutron_RouterGateway.yaml')),
-        P1(('OS_Neutron_SecurityGroup.yaml')),
-        P1(('OS_Nova_ServerGroup.yaml')),
-        P1(('OS_Nova_KeyPair.yaml')),
-        P1(('WR_Neutron_QoSPolicy.yaml')),
+        #P1(('WR_Neutron_ProviderNetRange.yaml')),
+        #P1(('WR_Neutron_ProviderNet.yaml')),
+        #P1(('OS_Cinder_Volume.yaml')),
+        #P1(('OS_Ceilometer_Alarm.yaml')),
+        #P1(('OS_Neutron_Port.yaml')),
+        #P1(('OS_Neutron_Net.yaml')),
+        #P1(('OS_Neutron_Subnet.yaml')),
+        #P1(('OS_Nova_Flavor.yaml')),
+        #P1(('OS_Neutron_FloatingIP.yaml')),
+        #P1(('OS_Neutron_Router.yaml')),
+        #P1(('OS_Neutron_RouterGateway.yaml')),
+        #P1(('OS_Neutron_SecurityGroup.yaml')),
+        #P1(('OS_Nova_ServerGroup.yaml')),
+        #P1(('OS_Nova_KeyPair.yaml')),
+        #P1(('WR_Neutron_QoSPolicy.yaml')),
+        #P1(('OS_Heat_Stack.yaml')),
+        #P1(('OS_Cinder_VolumeAttachment.yaml')),
+        P1(('OS_Nova_Server.yaml')),
+
     ])
 # can add test fixture to configure hosts to be certain storage backing
 def test_heat_template(template_name):
@@ -238,17 +254,13 @@ def test_heat_template(template_name):
 
 
 
-    # Another test step
+    # add test step
     LOG.tc_step("Create Heat stack using")
-    return_code, message = verify_basic_template(template_name)
+    return_code, msg = verify_basic_template(template_name)
 
     # Verify test results using assert
     LOG.tc_step("Verify test result")
-    assert return_code in [0, 1], message
-    # Can also add asserts to check the exact error message for negative test cases, i.e., return_code is 1
-
-    #Mark test end
-    #LOG.tc_end()
+    assert 0 == return_code, "Expected return code {}. Actual return code: {}; details: {}".format(0, return_code, msg)
 
 
 ########################################################################################################################
