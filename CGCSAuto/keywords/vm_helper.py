92c3a7b8f47299bcecf0c07ec7df148df1af2d6e
senthil <senthil.mukundakumar@windriver.com>
2016-06-22 15:29:57 -0500

    Added support for PCIPT & SRIOV

--- CGCSAuto/keywords/vm_helper.py
+++ CGCSAuto/keywords/vm_helper.py
@@ -328,7 +328,9 @@ def launch_vms_via_script(vm_type='avp', num_vms=1, launch_timeout=120, tenant_n
     """
     vif_mapping = {'vswitch': 'DPDKAPPS',
                    'avp': 'AVPAPPS',
-                   'virtio': 'VIRTIOAPPS'
+                   'virtio': 'VIRTIOAPPS',
+                   'sriov': 'SRIOVAPPS',
+                   'pcipt': 'PCIPTAPPS'
                    }
 
     if not tenant_name:
