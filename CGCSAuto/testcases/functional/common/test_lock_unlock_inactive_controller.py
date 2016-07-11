6335d5ab4343e23157e8248870a7040e0c9ace6f
wjia <wjia@windriver.com>
2016-07-11 10:41:25 -0400

    Fixed bugs in storage testcases. Added new mtc testcase check heartbeat after vim restart

--- CGCSAuto/testcases/functional/common/test_lock_unlock_inactive_controller.py
+++ CGCSAuto/testcases/functional/common/test_lock_unlock_inactive_controller.py
@@ -32,14 +32,14 @@ def test_lock_unlock_inactive_controller():
     """
     LOG.tc_step('Retrieve the standby controller from the lab')
     standby_controller = system_helper.get_standby_controller_name()
-    standby_controller_state = host_helper.get_hostshow_value(standby_controller,'administrative')
+    standby_controller_state = host_helper.get_hostshow_value(standby_controller, 'administrative')
 
     if standby_controller_state == 'locked':
         assert False, 'Standby controller {} is in locked state. When the test lab should have no locked compute ' \
                       'node'.format(standby_controller)
 
     # lock standby controller node and verify compute node is successfully locked
-    host_helper.lock_host(standby_controller, timeout=600)
+    host_helper.lock_host(standby_controller)
 
     lucky_compute_node_locked_state = host_helper.get_hostshow_value(standby_controller,'administrative')
     assert lucky_compute_node_locked_state == 'locked', 'Test Failed. Standby Controller {} should be in locked ' \
@@ -49,7 +49,7 @@ def test_lock_unlock_inactive_controller():
     sleep(20)
 
     # unlock standby controller node and verify controller node is successfully unlocked
-    host_helper.unlock_host(standby_controller, check_hypervisor_up=True)
+    host_helper.unlock_host(standby_controller)
     lucky_compute_node_unlocked_state = host_helper.get_hostshow_value(standby_controller,'administrative')
     assert lucky_compute_node_unlocked_state == 'unlocked', 'Test Failed. Standby Controller {} should be in unlocked ' \
                                                           'state but is not.'.format(standby_controller)
--- CGCSAuto/testcases/functional/mtc/test_heartbeat_after_vim_restart.py
+++ CGCSAuto/testcases/functional/mtc/test_heartbeat_after_vim_restart.py
@@ -8,10 +8,11 @@ from time import sleep
 
 from utils import table_parser
 from utils.tis_log import LOG
+from utils.ssh import ControllerClient
 from consts.cgcs import EventLogID, FlavorSpec
 from consts.timeout import EventLogTimeout
 from keywords import nova_helper, vm_helper, host_helper, system_helper
-
+from testfixtures.resource_mgmt import ResourceCleanup
 
 # heartbeat Type
 flavor_params = ['True']
@@ -34,41 +35,28 @@ def heartbeat_flavor_vm(request):
     flavor_id = nova_helper.create_flavor()[1]
     heartbeat_spec = {FlavorSpec.GUEST_HEARTBEAT: heartbeat}
     nova_helper.set_flavor_extra_specs(flavor=flavor_id, **heartbeat_spec)
+    ResourceCleanup.add('flavor', flavor_id,scope='module')
 
     # use volume to boot a vm by default
     vm_id = vm_helper.boot_vm(flavor=flavor_id)[1]
     events = system_helper.wait_for_events(EventLogTimeout.HEARTBEAT_ESTABLISH, strict=False, fail_ok=True,
                                            **{'Entity Instance ID': vm_id, 'Event Log ID': [
                                               EventLogID.HEARTBEAT_DISABLED, EventLogID.HEARTBEAT_ENABLED]})
-    if heartbeat == 'True':
-        assert events, "VM heartbeat is not enabled."
-        assert EventLogID.HEARTBEAT_ENABLED == events[0], "VM heartbeat failed to establish."
-    else:
-        assert not events, "Heartbeat event generated unexpectedly: {}".format(events)
 
     vm = {'id': vm_id,
           'heartbeat': heartbeat
           }
 
-    vm_host = nova_helper.get_vm_host(vm_id)
-
-    def unlock_host():
-        # must delete VM before flavors
-        vm_helper.delete_vms(vm_id, delete_volumes=True)
-        nova_helper.delete_flavors(flavor_ids=flavor_id, fail_ok=True)
-        # wait for hostname to be back in host list in nova
-        host_helper.wait_for_hypervisors_up(vm_host)
-        host_helper.wait_for_hosts_in_nova_compute(vm_host)
-    request.addfinalizer(unlock_host)
+    ResourceCleanup.add('vm', vm_id, scope='module')
 
     return vm
 
 
-def test_heartbeat_after_compute_lock(heartbeat_flavor_vm):
+def test_heartbeat_after_vim_restart(heartbeat_flavor_vm):
     """
-    from us63135_tc11: validate_heartbeat_works_after_compute_node_reboot
+    from us63135_tc11: validate_heartbeat_works_after_vim_restart
 
-    Verfiy heartbeat is still function after compute node where vm is located is locked
+    Verfiy heartbeat is still function after vim process is killed and restarted
 
     Args:
         - Nothing
@@ -77,10 +65,10 @@ def test_heartbeat_after_compute_lock(heartbeat_flavor_vm):
         1) Log on to active controller, add flavor with extension with heartbeat enabled.
         2) Instantiate VM and log into their VM consoles. Verify it's running on a compute node.
         3) Confirm that heartbeating is running in VM (check logs, and/or "ps -ef | fgrep guest-client").
-        4) Lock the compute node.
+        4) Kill the vim process on active controller
 
     Test Steps:
-        5) Verify VMs successfully migrate to the other compute node.
+        5) Verify VMs is still successfully running
         6) Log back into VM consoles, and verify that heartbeat is running
 
     Teardown:
@@ -91,15 +79,26 @@ def test_heartbeat_after_compute_lock(heartbeat_flavor_vm):
     vm_id = heartbeat_flavor_vm['id']
     heartbeat_type = heartbeat_flavor_vm['heartbeat']
 
-    LOG.tc_step("Reboot the compute node where the VM is located")
+    LOG.tc_step("Kill the nfv-vim.pid process on active controller")
     # find the compute node where the vm is located
-    vm_host = nova_helper.get_vm_host(vm_id)
 
-    host_helper.reboot_hosts(vm_host)
+    vm_host_table = system_helper.get_vm_topology_tables('servers')[0]
+    vm_host = table_parser.get_values(vm_host_table, 'host', ID=vm_id)[0]
+
+    #restart vim process
+    #run this from active controller
+    ssh_client = ControllerClient.get_active_controller()
+    controller = system_helper.get_active_controller_name()
+    first_cmd = "cat /var/volatile/run/nfv-vim.pid"
+    code, output = ssh_client.exec_sudo_cmd(first_cmd)
+    second_cmd = "kill -9 "+output
+    ssh_client.exec_sudo_cmd(second_cmd)
+
 
     with vm_helper.ssh_to_vm_from_natbox(vm_id) as vm_ssh:
 
-        LOG.tc_step("check heartbeat after compute lock")
+
+        LOG.tc_step("check heartbeat after restart vim nfv-vim.pid ")
         cmd = "ps -ef | grep [h]eartbeat | awk '{print $10}' "
         heartbeat_proc_shown = vm_ssh.wait_for_cmd_output(cmd, 'cgcs.heartbeat', timeout=10, strict=False, expt_timeout=3,
                                                           check_interval=2)
