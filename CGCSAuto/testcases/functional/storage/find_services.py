7c95e7ab2637c79f587fbfe82622dee2de6c7db8
Jordan Li <jordan.li@windriver.com>
2016-08-02 16:22:36 -0400

    Remove extra file

--- CGCSAuto/testcases/functional/storage/find_services.py
+++
@@ -1,7 +0,0 @@
-from keywords import system_helper
-from utils import table_parser
-
-
-def test_func():
-    print(system_helper.get_servicegroup_list(['service_group_name', 'uuid'], hostname='controller-1', state='disabled'))
-
