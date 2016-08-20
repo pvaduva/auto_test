da62f4d6b25642f45af805aae4ab9970bcff3fd5
Yang Liu <yliu12@yow-yliu12-vm.(none)>
2016-08-20 16:42:08 -0400

    revert to qemu-kvm. accidentally changed to kvm when updating 15.12 automation branch

--- CGCSAuto/testcases/functional/nova/test_vm_recovery.py
+++ CGCSAuto/testcases/functional/nova/test_vm_recovery.py
@@ -333,7 +333,7 @@ def test_vm_autorecovery_kill_host_kvm(heartbeat):
 
     LOG.tc_step("Kill the kvm processes on vm host: {}".format(target_host))
     with host_helper.ssh_to_host(target_host) as host_ssh:
-        exit_code, output = host_ssh.exec_sudo_cmd('killall -s KILL kvm')   # qemu-kvm for centos
+        exit_code, output = host_ssh.exec_sudo_cmd('killall -s KILL qemu-kvm')
         if not exit_code == 0:
             raise exceptions.SSHExecCommandFailed("Failed to kill host kvm processes. Details: {}".format(output))
 
