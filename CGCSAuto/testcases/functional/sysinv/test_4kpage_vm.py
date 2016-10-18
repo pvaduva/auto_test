4b8921df5a895fb79ddf42b89f22eee82f7fc130
Yang Liu <yliu12@yow-yliu12-vm.(none)>
2016-10-18 19:06:30 -0400

    fix issue in sysinv boot 4k vm testcases

--- CGCSAuto/testcases/functional/sysinv/test_4kpage_vm.py
+++ CGCSAuto/testcases/functional/sysinv/test_4kpage_vm.py
@@ -3,14 +3,12 @@
 # 58) Launch VMs using 4k-memory-pages
 ###
 
-
-from pytest import fixture, mark, skip
 import time
 
-from utils import cli
+from pytest import fixture, mark, skip
+
 from utils.ssh import ControllerClient
 from utils import table_parser
-from consts.auth import Tenant
 from consts.cgcs import FlavorSpec
 from utils.tis_log import LOG
 from keywords import nova_helper, vm_helper, host_helper, system_helper
@@ -18,6 +16,7 @@ from testfixtures.resource_mgmt import ResourceCleanup
 from testfixtures.recover_hosts import HostsToRecover
 
 
+@fixture(scope='module', autouse=True)
 def ensure_sufficient_4k_pages():
     """
     Check if there is enough 4k pages on any compute node on any processors is a bit hassle
@@ -104,7 +103,7 @@ def test_4k_page_vm(boot_source):
     # retrieve the correct table from the vm-topology
     nova_tab = table_parser.tables(con_ssh.exec_cmd('vm-topology --show servers',expect_timeout=30)[1],
                                    combine_multiline_entry=False)[0]
-    print(nova_tab)
+
     vm_row = [row for row in nova_tab['values'] if row[1] == vm_id][0]
     attribute = vm_row[11].split(', ')
 
@@ -112,17 +111,17 @@ def test_4k_page_vm(boot_source):
                                         "However, output is {} ".format(attribute[2])
 
 
-@mark.parametrize(('storage_backing', 'ephemeral', 'swap', 'cpu_pol', 'vcpus', 'vm_type', 'block_mig'), [
-    mark.p2(('local_image', 0, 0, None, 1, 'volume', True)),
-    mark.p1(('local_image', 0, 0, None, 1, 'image', True)),
-    mark.p1(('local_image', 0, 0, None, 3, 'image', True)),
-    mark.p2(('local_lvm', 0, 0, None, 1, 'volume', True)),
-    mark.p2(('local_lvm', 0, 0, None, 1, 'image', True)),
-    mark.p2(('remote', 1, 1, None, 1, 'volume', True)),
-    mark.p2(('remote', 0, 0, None, 2, 'image', True)),
+@mark.parametrize(('storage_backing', 'ephemeral', 'swap', 'cpu_pol', 'vcpus', 'vm_type'), [
+    mark.p2(('local_image', 0, 0, None, 1, 'volume')),
+    mark.p1(('local_image', 0, 0, None, 1, 'image')),
+    mark.p1(('local_image', 0, 0, None, 3, 'image')),
+    mark.p2(('local_lvm', 0, 0, None, 1, 'volume')),
+    mark.p2(('local_lvm', 0, 0, None, 1, 'image')),
+    mark.p2(('remote', 1, 1, None, 1, 'volume')),
+    mark.p2(('remote', 0, 0, None, 2, 'image')),
 ])
-def test_live_migrate_vm_positive(storage_backing, ephemeral, swap, cpu_pol, vcpus, vm_type, block_mig,
-                                  hosts_per_stor_backing):
+def test_live_migrate_4k_vm_positive(storage_backing, ephemeral, swap, cpu_pol, vcpus, vm_type,
+                                     hosts_per_stor_backing):
     if len(hosts_per_stor_backing[storage_backing]) < 2:
         skip("Less than two hosts have {} storage backing".format(storage_backing))
 
@@ -144,24 +143,18 @@ def test_live_migrate_vm_positive(storage_backing, ephemeral, swap, cpu_pol, vcp
     assert attribute[2] == 'pgsize:4K', "expected result to be pgsize:4K. " \
                                         "However, output is {} ".format(attribute[2])
 
-    # start live migration
-    prev_vm_host = nova_helper.get_vm_host(vm_id)
-    LOG.tc_step("Live migrate VM and ensure it succeeded")
-    block_mig = True if vm_type == 'image' else False
-    code, output = vm_helper.live_migrate_vm(vm_id, block_migrate=block_mig)
-    assert 0 == code, "Live migrate is not successful. Details: {}".format(output)
-
-    post_vm_host = nova_helper.get_vm_host(vm_id)
-    assert prev_vm_host != post_vm_host
+    LOG.tc_step("Attempt to live migrate VM")
+    vm_helper.live_migrate_vm(vm_id)
+    vm_helper.wait_for_vm_pingable_from_natbox(vm_id)
 
 
 @mark.parametrize(('storage_backing', 'ephemeral', 'swap', 'cpu_pol', 'vcpus', 'vm_type'), [
     mark.p2(('local_image', 0, 0, None, 1, 'volume')),
-    mark.p2(('local_lvm', 0, 0, None, 1, 'volume')),
-    mark.p2(('remote', 0, 0, None, 2, 'volume')),
-    mark.p1(('local_image', 0, 0, None, 1, 'image')),
+    mark.p2(('local_lvm', 0, 1, None, 1, 'volume')),
+    mark.p2(('remote', 1, 0, None, 2, 'volume')),
+    mark.p1(('local_image', 1, 0, None, 1, 'image')),
     mark.p2(('local_lvm', 0, 0, None, 1, 'image')),
-    mark.p2(('remote', 0, 0, None, 2, 'image')),
+    mark.p2(('remote', 0, 1, None, 2, 'image')),
 ])
 def test_cold_migrate_4k_vm(storage_backing, ephemeral, swap, cpu_pol, vcpus, vm_type, hosts_per_stor_backing):
     if len(hosts_per_stor_backing[storage_backing]) < 2:
@@ -178,22 +171,17 @@ def test_cold_migrate_4k_vm(storage_backing, ephemeral, swap, cpu_pol, vcpus, vm
     LOG.tc_step("Verify cpu info for vm {} via vm-topology.".format(vm_id))
     con_ssh = ControllerClient.get_active_controller()
     # retrieve the correct table from the vm-topology
-    nova_tab = table_parser.tables(con_ssh.exec_cmd('vm-topology --show servers',expect_timeout=30)[1],
+    nova_tab = table_parser.tables(con_ssh.exec_cmd('vm-topology --show servers', expect_timeout=30)[1],
                                    combine_multiline_entry=False)[0]
-    print(nova_tab)
+
     vm_row = [row for row in nova_tab['values'] if row[1] == vm_id][0]
     attribute = vm_row[11].split(', ')
     assert attribute[2] == 'pgsize:4K', "expected result to be pgsize:4K. " \
                                         "However, output is {} ".format(attribute[2])
 
-    prev_vm_host = nova_helper.get_vm_host(vm_id)
     LOG.tc_step("Cold migrate VM and ensure it succeeded")
-    # block_mig = True if boot_source == 'image' else False
-    code, output = vm_helper.cold_migrate_vm(vm_id)
-    assert 0 == code, "Cold migrate is not successful. Details: {}".format(output)
-
-    post_vm_host = nova_helper.get_vm_host(vm_id)
-    assert prev_vm_host != post_vm_host
+    vm_helper.cold_migrate_vm(vm_id)
+    vm_helper.wait_for_vm_pingable_from_natbox(vm_id)
 
 
 def _boot_vm_under_test(storage_backing, ephemeral, swap, cpu_pol, vcpus, vm_type):
@@ -213,12 +201,9 @@ def _boot_vm_under_test(storage_backing, ephemeral, swap, cpu_pol, vcpus, vm_typ
     LOG.tc_step("Add following extra specs: {}".format(specs))
     nova_helper.set_flavor_extra_specs(flavor=flavor_id, **specs)
 
-    # verify there is enough 4k pages on compute nodes to create 4k page flavor
-    ensure_sufficient_4k_pages()
-
     boot_source = 'volume' if vm_type == 'volume' else 'image'
     LOG.tc_step("Boot a vm from {}".format(boot_source))
-    vm_id = vm_helper.boot_vm('mig', flavor=flavor_id, source=boot_source)[1]
+    vm_id = vm_helper.boot_vm('4k_mig', flavor=flavor_id, source=boot_source)[1]
     ResourceCleanup.add('vm', vm_id)
 
     if vm_type == 'image_with_vol':
