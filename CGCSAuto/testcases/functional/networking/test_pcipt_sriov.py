ebe3002cf7d517bbc28fdfa45dc064cd25f4471e
Marvin Huang <marvin.huang@windriver.com>
2017-02-03 10:32:40 -0500

    fix identation for testcases/functional/networking/test_pcipt_sriov.py

--- CGCSAuto/testcases/functional/networking/test_pcipt_sriov.py
+++ CGCSAuto/testcases/functional/networking/test_pcipt_sriov.py
@@ -103,76 +103,76 @@ def vif_model_check(request):
     return vif_model, base_vm, flavor_id, nics_to_test, seg_id, net_type, pnet_id
 
 
-    @mark.p2
-    def test_evacuate_pci_vm(vif_model_check):
-        """
-        Test evacuate vm with multiple ports on same network
+@mark.p2
+def test_evacuate_pci_vm(vif_model_check):
+    """
+    Test evacuate vm with multiple ports on same network
 
-        Args:
-            vifs (tuple): vif models to test. Used when booting vm with tenant network nics info
-            net_setups_ (tuple): flavor, networks to use and base vm info
+    Args:
+        vifs (tuple): vif models to test. Used when booting vm with tenant network nics info
+        net_setups_ (tuple): flavor, networks to use and base vm info
 
-        Setups:
-            - create a flavor with dedicated cpu policy (module)
-            - choose one tenant network and one internal network to be used by test (module)
-            - boot a base vm - vm1 with above flavor and networks, and ping it from NatBox (module)
-            - Boot a vm under test - vm2 with above flavor and with multiple ports on same tenant network with base vm,
-            and ping it from NatBox     (class)
-            - Ping vm2's own data network ips       (class)
-            - Ping vm2 from vm1 to verify management and data networks connection   (class)
+    Setups:
+        - create a flavor with dedicated cpu policy (module)
+        - choose one tenant network and one internal network to be used by test (module)
+        - boot a base vm - vm1 with above flavor and networks, and ping it from NatBox (module)
+        - Boot a vm under test - vm2 with above flavor and with multiple ports on same tenant network with base vm,
+        and ping it from NatBox     (class)
+        - Ping vm2's own data network ips       (class)
+        - Ping vm2 from vm1 to verify management and data networks connection   (class)
 
-        Test Steps:
-            - Reboot vm2 host
-            - Wait for vm2 to be evacuated to other host
-            - Wait for vm2 pingable from NatBox
-            - Verify ping from vm1 to vm2 over management and data networks still works
+    Test Steps:
+        - Reboot vm2 host
+        - Wait for vm2 to be evacuated to other host
+        - Wait for vm2 pingable from NatBox
+        - Verify ping from vm1 to vm2 over management and data networks still works
 
-        Teardown:
-            - Delete created vms and flavor
-        """
-        vif_model, base_vm, flavor_id, nics_to_test, seg_id, net_type, pnet_id = vif_model_check
+    Teardown:
+        - Delete created vms and flavor
+    """
+    vif_model, base_vm, flavor_id, nics_to_test, seg_id, net_type, pnet_id = vif_model_check
 
-        LOG.tc_step("Boot a vm with {} vif model on {} net".format(vif_model, net_type))
-        res, vm_id, err, vol_id = vm_helper.boot_vm(name=vif_model, flavor=flavor_id, nics=nics_to_test)
-        if vm_id:
-            ResourceCleanup.add('vm', vm_id, del_vm_vols=False)
-            pass
-        if vol_id:
-            ResourceCleanup.add('volume', vol_id)
-            pass
-        assert 0 == res, "VM is not booted successfully. Error: {}".format(err)
+    LOG.tc_step("Boot a vm with {} vif model on {} net".format(vif_model, net_type))
+    res, vm_id, err, vol_id = vm_helper.boot_vm(name=vif_model, flavor=flavor_id, nics=nics_to_test)
+    if vm_id:
+        ResourceCleanup.add('vm', vm_id, del_vm_vols=False)
+        pass
+    if vol_id:
+        ResourceCleanup.add('volume', vol_id)
+        pass
+    assert 0 == res, "VM is not booted successfully. Error: {}".format(err)
 
-        vm_helper.wait_for_vm_pingable_from_natbox(vm_id, fail_ok=False)
+    vm_helper.wait_for_vm_pingable_from_natbox(vm_id, fail_ok=False)
 
-        if 'pci-passthrough' == vif_model:
-            LOG.tc_step("Add vlan to pci-passthrough interface for VM.")
-            vm_helper.add_vlan_for_vm_pcipt_interfaces(vm_id=vm_id, net_seg_id=seg_id)
+    if 'pci-passthrough' == vif_model:
+        LOG.tc_step("Add vlan to pci-passthrough interface for VM.")
+        vm_helper.add_vlan_for_vm_pcipt_interfaces(vm_id=vm_id, net_seg_id=seg_id)
 
-        LOG.tc_step("Ping vm over mgmt and {} nets from base vm".format(net_type))
-        vm_helper.ping_vms_from_vm(from_vm=base_vm, to_vms=vm_id, net_types=['mgmt', net_type], vlan_zero_only=True)
+    LOG.tc_step("Ping vm over mgmt and {} nets from base vm".format(net_type))
+    vm_helper.ping_vms_from_vm(from_vm=base_vm, to_vms=vm_id, net_types=['mgmt', net_type], vlan_zero_only=True)
 
-        host = nova_helper.get_vm_host(vm_id)
+    host = nova_helper.get_vm_host(vm_id)
 
-        LOG.tc_step("Reboot vm host {}".format(host))
-        host_helper.reboot_hosts(host, wait_for_reboot_finish=False)
-        HostsToRecover.add(host, scope='function')
+    LOG.tc_step("Reboot vm host {}".format(host))
+    host_helper.reboot_hosts(host, wait_for_reboot_finish=False)
+    HostsToRecover.add(host, scope='function')
 
-        LOG.tc_step("Wait for vm to reach ERROR or REBUILD state with best effort")
-        vm_helper._wait_for_vms_values(vm_id, values=[VMStatus.ERROR, VMStatus.REBUILD], fail_ok=True, timeout=120)
+    LOG.tc_step("Wait for vm to reach ERROR or REBUILD state with best effort")
+    vm_helper._wait_for_vms_values(vm_id, values=[VMStatus.ERROR, VMStatus.REBUILD], fail_ok=True, timeout=120)
 
-        LOG.tc_step("Verify vm is evacuated to other host")
-        vm_helper._wait_for_vm_status(vm_id, status=VMStatus.ACTIVE, timeout=300, fail_ok=False)
-        post_evac_host = nova_helper.get_vm_host(vm_id)
-        assert post_evac_host != host, "VM is on the same host after original host rebooted."
+    LOG.tc_step("Verify vm is evacuated to other host")
+    vm_helper._wait_for_vm_status(vm_id, status=VMStatus.ACTIVE, timeout=300, fail_ok=False)
+    post_evac_host = nova_helper.get_vm_host(vm_id)
+    assert post_evac_host != host, "VM is on the same host after original host rebooted."
 
-        vm_helper.wait_for_vm_pingable_from_natbox(vm_id)
+    vm_helper.wait_for_vm_pingable_from_natbox(vm_id)
 
-        if 'pci-passthrough' == vif_model:
-            LOG.tc_step("Add vlan to pci-passthrough interface for VM again after evacuation due to interface change.")
-            vm_helper.add_vlan_for_vm_pcipt_interfaces(vm_id=vm_id, net_seg_id=seg_id)
+    if 'pci-passthrough' == vif_model:
+        LOG.tc_step("Add vlan to pci-passthrough interface for VM again after evacuation due to interface change.")
+        vm_helper.add_vlan_for_vm_pcipt_interfaces(vm_id=vm_id, net_seg_id=seg_id)
 
-        LOG.tc_step("Check vm still pingable over mgmt, and {} nets after evacuation".format(net_type))
-        vm_helper.ping_vms_from_vm(from_vm=base_vm, to_vms=vm_id, net_types=['mgmt', net_type], vlan_zero_only=True)
+    LOG.tc_step("Check vm still pingable over mgmt, and {} nets after evacuation".format(net_type))
+    vm_helper.ping_vms_from_vm(from_vm=base_vm, to_vms=vm_id, net_types=['mgmt', net_type], vlan_zero_only=True)
 
 
 @mark.p3
