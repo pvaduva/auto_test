b5b3242ef726cba99d9f375bcf086fd7c66d267d
Yang Liu <yliu12@windriver.com>
2017-03-02 14:14:51 -0500

    update automation scripts due to behavior change in newton

--- CGCSAuto/consts/cli_errs.py
+++ CGCSAuto/consts/cli_errs.py
@@ -23,7 +23,7 @@ class NumaErr:
 class MinCPUErr:
     VAL_LARGER_THAN_VCPUS = "min_vcpus must be less than or equal to the flavor vcpus value"
     VAL_LESS_THAN_1 = "min_vcpus must be greater than or equal to 1"
-    CPU_POLICY_NOT_DEDICATED = "min_vcpus is only valid when hw:cpu_policy is 'dedicated'"
+    CPU_POLICY_NOT_DEDICATED = "min_vcpus is only valid when hw:cpu_policy is dedicated"
 
 
 class ScaleErr:
