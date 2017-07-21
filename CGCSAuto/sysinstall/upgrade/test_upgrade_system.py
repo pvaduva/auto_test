d47b3d058b1f213d34be9423121d41d44fcca572
Anuejyan Manokeran <anujeyan.manokeran@windriver.com>
2017-07-21 11:06:24 -0400

    Delay before and after swacts for service to come up.

--- CGCSAuto/sysinstall/upgrade/test_upgrade_system.py
+++ CGCSAuto/sysinstall/upgrade/test_upgrade_system.py
@@ -40,10 +40,7 @@ def test_system_upgrade(upgrade_setup, check_system_health_query_upgrade):
     LOG.info("Host controller-1 unlocked after upgrade......")
 
     time.sleep(60)
-
-    # Swact to standby controller-1
-    LOG.tc_step("Swacting to controller-1 .....")
-    # Before Swacting ensure the controller-1 is in available state
+      # Before Swacting ensure the controller-1 is in available state
     if not host_helper.wait_for_host_states("controller-1", timeout=360, fail_ok=True,
                                             operational=HostOperationalState.ENABLED,
                                             availability=HostAvailabilityState.AVAILABLE):
@@ -51,12 +48,14 @@ def test_system_upgrade(upgrade_setup, check_system_health_query_upgrade):
               "within  the specified timeout"
         assert False, err_msg
 
+    # Swact to standby contime.sleep(60)  troller-1
+    LOG.tc_step("Swacting to controller-1 .....")
     rc, output = host_helper.swact_host(hostname="controller-0")
     assert rc == 0, "Failed to swact: {}".format(output)
     LOG.info("Swacted and  controller-1 has become active......")
 
     active_controller = system_helper.get_active_controller_name()
-
+    time.sleep(60)
     # upgrade  controller-0
     LOG.tc_step("Upgrading  controller-0......")
     controller0 = lab['controller-0']
