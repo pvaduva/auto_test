9e0cd0cc952848ac6a1c410a924c74852a029e88
Yang Liu <yang.liu@windriver.com>
2017-07-25 20:51:51 -0400

    update cpe to aio-dx in report

--- CGCSAuto/utils/lab_info.py
+++ CGCSAuto/utils/lab_info.py
@@ -131,7 +131,7 @@ def _get_sys_type(labname=None, log_dir=None, con_ssh=None):
     sys_type = "{}+{}+{}".format(len(controllers), len(computes), len(storages)).replace('+0', '')
 
     if '+' not in sys_type:
-        sys_type = 'CPE - {} nodes'.format(sys_type)
+        sys_type = 'AIO-DX' if sys_type == '2' else 'AIO-SX'
 
     if close:
         con_ssh.close()
