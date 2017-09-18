d0f4bcc0acb7f9873332e11b1af13de36850a0d4
Maria Yousaf <maria.yousaf@windriver.com>
2017-09-18 11:46:01 -0400

    Disable test_ceph_osd_process_kill until test can be made more reliable.  Currently fails 1 out of 10 attempts because the OSD recovers faster than the system can detect a failure has occurred.  Needs further investigation to determine how to fix.

--- CGCSAuto/testcases/functional/storage/ceph/test_ceph.py
+++ CGCSAuto/testcases/functional/storage/ceph/test_ceph.py
@@ -23,7 +23,7 @@ PROC_RESTART_TIME = 30          # number of seconds between process restarts
 # Tested on PV1.  Runtime: 278.40  Date: Aug 2nd, 2017.  Status: Pass
 @mark.nightly
 @mark.usefixtures('ceph_precheck')
-def test_ceph_osd_process_kill():
+def _test_ceph_osd_process_kill():
     """
     us69932_tc1_ceph_osd_process_kill from us69932_ceph_monitoring.odt
 
