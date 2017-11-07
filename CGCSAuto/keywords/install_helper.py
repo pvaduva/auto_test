a22f41b8723440cd8ab0d51366050cab210f64eb
Marvin Huang <marvin.huang@windriver.com>
2017-11-06 19:07:10 -0500

    finalize back-restore for storage-lab

--- CGCSAuto/keywords/install_helper.py
+++ CGCSAuto/keywords/install_helper.py
@@ -21,11 +21,15 @@ from utils.tis_log import LOG
 from utils import local_host
 from consts.auth import Tenant, CliAuth
 import setups
+import configparser
+
 
 UPGRADE_LOAD_ISO_FILE = "bootimage.iso"
 BACKUP_USB_MOUNT_POINT = '/media/wrsroot'
 TUXLAB_BARCODES_DIR = "/export/pxeboot/vlm-boards/"
 CENTOS_INSTALL_REL_PATH = "export/dist/isolinux/"
+
+lab_ini_info = {}
 
 def get_ssh_public_key():
     return local_host.get_ssh_key()
@@ -1875,7 +1879,7 @@ def export_image(image_id, backup_dest='usb', backup_dest_path=BackupRestore.USB
     img_backup_cmd = 'image-backup export ' + image_id
     # temp sleep wait for image-backup to complete
     con_ssh.exec_sudo_cmd(img_backup_cmd, expect_timeout=300)
-    src_files = "/opt/backups/image_{}*.tgz".format(image_id)
+    src_file = "/opt/backups/image_{}*.tgz".format(image_id)
     if backup_dest == 'local':
         if dest_server:
             if dest_server.ssh_conn.exec_cmd("test -e {}".format(backup_dest_path))[0] != 0:
@@ -1884,16 +1888,17 @@ def export_image(image_id, backup_dest='usb', backup_dest_path=BackupRestore.USB
             if local_host.exec_cmd(["test", '-e',  "{}".format(backup_dest_path)])[0] != 0:
                 local_host.exec_cmd("mkdir -p {}".format(backup_dest_path))
 
-        common.scp_from_active_controller_to_test_server(src_files, backup_dest_path, is_dir=False, multi_files=True)
+        common.scp_from_active_controller_to_test_server(src_file, backup_dest_path, is_dir=False, multi_files=True)
 
         LOG.info("Verifying if image backup files are copied to destination")
+        base_name_src = os.path.basename(src_file)
         if dest_server:
-            rc, output = dest_server.ssh_conn.exec_cmd("ls {}/{}*.tgz".format(backup_dest_path, src_files ))
+            rc, output = dest_server.ssh_conn.exec_cmd("ls {}/{}".format(backup_dest_path, base_name_src))
         else:
-            rc, output = local_host.exec_cmd(["ls {}/{}*.tgz".format(backup_dest_path, src_files)])
+            rc, output = local_host.exec_cmd("ls {}/{}".format(backup_dest_path, base_name_src))
 
         if rc != 0:
-            err_msg = "Failed to scp image backup files {} to local destination: {}".format(src_files, output)
+            err_msg = "Failed to scp image backup files {} to local destination: {}".format(src_file, output)
             LOG.info(err_msg)
             if fail_ok:
                 return 2, err_msg
@@ -1930,7 +1935,7 @@ def export_image(image_id, backup_dest='usb', backup_dest_path=BackupRestore.USB
                     raise exceptions.CommonError(err_msg)
         else:
             err_msg = "USB {} does not have mount point; cannot copy  backup files {} to USB"\
-                .format(copy_to_usb, src_files)
+                .format(copy_to_usb, src_file)
             LOG.info(err_msg)
             if fail_ok:
                 return 2, err_msg
@@ -2191,3 +2196,28 @@ def update_auth_url(ssh_con, region=None, fail_ok=True):
     CliAuth.set_vars(**setups.get_auth_via_openrc(ssh_con))
     Tenant._set_url(CliAuth.get_var('OS_AUTH_URL'))
     Tenant._set_region(CliAuth.get_var('OS_REGION_NAME'))
+
+
+def get_lab_info(barcode):
+    global lab_ini_info
+
+    if lab_ini_info and barcode in lab_ini_info:
+        return lab_ini_info[barcode]
+
+    ini_file_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'sanityrefresh/labinstall/node_info')
+
+    ini_file = os.path.join(os.path.realpath(ini_file_dir), '{}.ini'.format(barcode))
+    LOG.debug('ini file:{}, barcode:{}'.format(ini_file, barcode))
+
+    conf_parser = configparser.ConfigParser()
+    conf_parser.read(ini_file)
+
+    settings = dict(conf_parser.defaults())
+    for ss in conf_parser.sections():
+        settings.update(dict(conf_parser[ss]))
+
+    LOG.debug('settings in ini file:{}'.format(settings))
+
+    lab_ini_info[barcode] = settings
+
+    return settings
