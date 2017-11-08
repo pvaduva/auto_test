d18777a4754336211b8cab6319834004d545a843
Sebastien Papineau <spapinea@yow-spapinea-lx.wrs.com>
2017-11-08 09:24:21 -0500

    Final functest version after code review and functional tests

--- CGCSAuto/utils/jenkins_utils/create_log_dir.py
+++ CGCSAuto/utils/jenkins_utils/create_log_dir.py
@@ -18,6 +18,21 @@ def create_log_dir(lab, logs_dir=None):
     return session_dir
 
 
+def create_functest_log_dir(logs_dir=None):
+
+    logs_dir = logs_dir if logs_dir else os.path.expanduser("~")
+    if '/AUTOMATION_LOGS' in logs_dir:
+        logs_dir = logs_dir.split(sep='/AUTOMATION_LOGS')[0]
+    if not logs_dir.endswith('/'):
+        logs_dir += '/'
+
+    session_dir = logs_dir + "AUTOMATION_LOGS/" + 'functest/' + strftime('%Y%m%d%H%M')
+    os.makedirs(session_dir, exist_ok=True)
+
+    return session_dir
+
+
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-d', '--dir', action='store', type='string', dest='logs_dir',
