f317c1d6b4a7b14c1af783b29f785925cee22a4e
Maria Yousaf <maria.yousaf@windriver.com>
2017-11-10 14:33:53 -0500

    Modify tests to use collect_kpis option.

--- CGCSAuto/testcases/kpi/install_kpis.py
+++ CGCSAuto/testcases/kpi/install_kpis.py
@@ -36,79 +36,91 @@ def heat_precheck():
         skip("Heat is not used to setup this lab")
 
 
+@mark.kpi
 @mark.usefixtures("drdb_precheck")
-def test_drbd_kpi():
+def test_drbd_kpi(collect_kpi):
     """
     This test extracts the DRDB sync time from log files
     """
 
+    if not collect_kpi:
+        skip("KPI only test.  Skip due to kpi collection is not enabled")
+
     lab_name = ProjVar.get_var('LAB_NAME')
     log_path = DRBDSync.LOG_PATH
     kpi_name = DRBDSync.NAME
-    local_kpi_file = "~/AUTOMATION_LOGS/kpi/drbd.txt"
     end_pattern = DRBDSync.GREP_PATTERN
     python_pattern = DRBDSync.PYTHON_PATTERN
 
-    kpi_log_parser.record_kpi(local_kpi_file=local_kpi_file, kpi_name=kpi_name,
+    kpi_log_parser.record_kpi(local_kpi_file=collect_kpi, kpi_name=kpi_name,
                               log_path=log_path, python_pattern=python_pattern,
                               lab_name=lab_name,
                               average_for_all=True, end_pattern=end_pattern)
 
 
-def test_config_controller_kpi():
+@mark.kpi
+def test_config_controller_kpi(collect_kpi):
     """
     This test extracts the time required to run config_controller.
     """
+
+    if not collect_kpi:
+        skip("KPI only test.  Skip due to kpi collection is not enabled")
 
     lab_name = ProjVar.get_var("LAB_NAME")
     log_path = ConfigController.LOG_PATH
     kpi_name = ConfigController.NAME
     host = "controller-0"
-    local_kpi_file = "~/AUTOMATION_LOGS/kpi/config_controller.txt"
     start_pattern = ConfigController.START
     end_pattern = ConfigController.END
 
-    kpi_log_parser.record_kpi(local_kpi_file=local_kpi_file, kpi_name=kpi_name,
+    kpi_log_parser.record_kpi(local_kpi_file=collect_kpi, kpi_name=kpi_name,
                               log_path=log_path, lab_name=lab_name, host=host,
                               start_pattern=start_pattern,
                               end_pattern=end_pattern, sudo=True, topdown=True)
 
 
-def test_lab_setup_kpi():
+@mark.kpi
+def test_lab_setup_kpi(collect_kpi):
     """
     This test extracts the time required to run lab_setup.sh only.
     """
+
+    if not collect_kpi:
+        skip("KPI only test.  Skip due to kpi collection is not enabled")
 
     lab_name = ProjVar.get_var("LAB_NAME")
     log_path = LabSetup.LOG_PATH
     kpi_name = LabSetup.NAME
     host = "controller-0"
-    local_kpi_file = "~/AUTOMATION_LOGS/kpi/lab_setup.txt"
     start_pattern = LabSetup.START
     end_pattern = LabSetup.END
 
-    kpi_log_parser.record_kpi(local_kpi_file=local_kpi_file, kpi_name=kpi_name,
+    kpi_log_parser.record_kpi(local_kpi_file=collect_kpi, kpi_name=kpi_name,
                               log_path=log_path, lab_name=lab_name, host=host,
                               start_pattern=start_pattern,
                               end_pattern=end_pattern, sudo=True, topdown=True)
 
 
+@mark.kpi
 @mark.usefixtures("heat_precheck")
-def test_heat_kpi():
+def test_heat_kpi(collect_kpi):
     """
     Time to launch heat stacks.  Only applies to labs where .heat_resources is
     present.
     """
 
+    if not collect_kpi:
+        skip("KPI only test.  Skip due to kpi collection is not enabled")
+
     lab_name = ProjVar.get_var("LAB_NAME")
     log_path = HeatStacks.LOG_PATH
     kpi_name = HeatStacks.NAME
     host = "controller-0"
-    local_kpi_file = "~/AUTOMATION_LOGS/kpi/heat_stacks.txt"
     start_pattern = HeatStacks.START
     end_pattern = HeatStacks.END
 
-    kpi_log_parser.record_kpi(local_kpi_file=local_kpi_file, kpi_name=kpi_name,
+    kpi_log_parser.record_kpi(local_kpi_file=collect_kpi, kpi_name=kpi_name,
                               log_path=log_path, lab_name=lab_name, host=host,
                               start_pattern=start_pattern,
                               end_pattern=end_pattern, sudo=True, topdown=True,
