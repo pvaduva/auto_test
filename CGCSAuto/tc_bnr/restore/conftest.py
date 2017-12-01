0fcb76f860df6a948d148d2916dd49316775b60b
Yang Liu <yang.liu@windriver.com>
2017-12-01 15:02:03 -0500

    refactor backup/restore options

--- CGCSAuto/tc_bnr/restore/conftest.py
+++ CGCSAuto/tc_bnr/restore/conftest.py
@@ -18,10 +18,11 @@ from testfixtures.pre_checks_and_configs import *
 def pytest_configure(config):
     # Lab install params
     lab_arg = config.getoption('lab')
-    backup_src = config.getoption('backup_src')
-    backup_src_path = config.getoption('backup_src_path')
+    use_usb = config.getoption('usb')
+    backup_src_path = config.getoption('backup_path')
     backup_build_id = config.getoption('backup_build_id')
     backup_builds_dir = config.getoption('backup_builds_dir')
+    backup_src = 'USB' if use_usb else 'local'
     setups.set_install_params(lab=lab_arg, skip_labsetup=None, resume=None, installconf_path=None,
                               controller0_ceph_mon_device=None, controller1_ceph_mon_device=None, ceph_mon_gib=None)
     RestoreVars.set_restore_vars(backup_src=backup_src, backup_src_path=backup_src_path,
@@ -31,31 +32,11 @@ def pytest_configure(config):
     ProjVar.set_var(SOURCE_CREDENTIAL=Tenant.ADMIN)
 
 
-@pytest.fixture(scope='session', autouse=True)
-def setup_test_session():
-    """
-
-    """
-    LOG.tc_func_start("RESTORE_TEST")
-
-
-def pytest_collectstart():
-    """
-    Set up the ssh session at collectstart. Because skipif condition is evaluated at the collecting test cases phase.
-    """
-    global con_ssh
-    lab = ProjVar.get_var("LAB")
-    lab_name = lab['name']
-    con_ssh = None
-
-
 def pytest_runtest_teardown(item):
     lab = InstallVars.get_install_var('LAB')
-    hostnames = [ k for k, v in lab.items() if isinstance(v, node.Node)]
+    hostnames = [k for k, v in lab.items() if isinstance(v, node.Node)]
     vlm_helper.unreserve_hosts(hostnames)
     con_ssh = ControllerClient.get_active_controller(lab['short_name'])
     # Delete any backup files from /opt/backups
     con_ssh.exec_sudo_cmd("rm -rf /opt/backups/*")
     con_ssh.flush()
-
-
