d465ff80014a32813e5c212fc185b03a66dcc3eb
Yang Liu <yang.liu@windriver.com>
2018-01-05 12:57:59 -0500

    added stress bmc power-cycle and reset testcases.

--- CGCSAuto/testcases/stress/mtc/test_sensors_added.py
+++ CGCSAuto/testcases/stress/mtc/test_sensors_added.py
@@ -192,6 +192,106 @@ def test_sensorgroup_action_taken(host,
     HOST = ''
 
 
+@mark.parametrize(('host', 'eventlevel', 'action',
+                   'expected_host_state',
+                   'expected_alarm_state',
+                   'event_type',
+                   'suppressionlevel'), [
+                      ('compute-1', 'action_critical', 'power-cycle', 'power-off', 'yes_alarm', 'cr', 'unsuppressed'),
+                      ('compute-0', 'action_critical', 'reset', 'offline', 'yes_alarm', 'nr', 'unsuppressed'),
+                  ])
+def test_sensorgroup_power_cycle(host,
+                                  eventlevel,
+                                  action,
+                                  expected_host_state,
+                                  expected_alarm_state,
+                                  event_type,
+                                  suppressionlevel, sensor_data_fit):
+    """
+    Verify that the sensorgroup action taken for an event is valid.
+
+    Test Steps:
+        - Get a sensorgroup to test
+        - Set the event level and expected action
+        - trigger an out-of-scope event for that sensorgroup
+        - verify that the expected action is taken
+
+    """
+    bmc_hosts = sensor_data_fit
+    if host not in bmc_hosts:
+        skip("{} is not configured with BMC sensor".format(host))
+
+    global HOST
+    HOST = host
+
+    if suppressionlevel == 'suppressed':
+        # global SUPPRESSED
+        # SUPPRESSED = host
+        suppress = True
+    else:
+        suppress = False
+
+    expt_severity = eventlevel.split('_')[-1] if 'yes' in expected_alarm_state else None
+
+    # Get a sensor to validate
+    sensorgroup_name = random.choice(bmc_helper.get_sensor_names(host, sensor_group=True))
+    for i in range(4):
+        LOG.info("################## iter {} #########################".format(i+1))
+        LOG.tc_step("Validating that sensorgroup: {} "
+                    "can be set to sensor action: {} "
+                    "for event level: {}".format(sensorgroup_name, action,
+                                                 eventlevel))
+
+        # Set the event level and action
+        bmc_helper.modify_sensorgroup(host, sensorgroup_name, value='name', suppress=suppress, audit_interval=10,
+                                      **{eventlevel: action})
+
+        # Get a sensor that is part of the sensorgroup
+        sensor_name = bmc_helper.get_first_sensor_from_sensorgroup(sensorgroup_name, host)
+        entity_id = 'host={}.sensor={}'.format(host, sensor_name)
+
+        LOG.tc_step("Trigger event for sensorgroup: {} and sensor name: {}".
+                    format(sensorgroup_name, sensor_name))
+        if action in ['power-cycle', 'reset']:
+            HostsToRecover.add(host)
+
+        start_time = common.get_date_in_format()
+        bmc_helper.trigger_event(host, sensor_name, event_type)
+
+        LOG.tc_step("Check sensor status and alarm for {}".format(sensor_name))
+        if expected_alarm_state == 'yes_alarm':
+            system_helper.wait_for_alarm(alarm_id=EventLogID.BMC_SENSOR_ACTION, entity_id=entity_id,
+                                         severity=expt_severity, timeout=60, strict=False, fail_ok=False)
+        else:
+            events = system_helper.wait_for_events(timeout=60, num=10, event_log_id=EventLogID.BMC_SENSOR_ACTION,
+                                                   entity_instance_id=entity_id, start=start_time, state='log',
+                                                   severity=expt_severity, fail_ok=True, strict=False)
+            if expected_alarm_state == 'yes_log':
+                assert events, "No event log found for {} {} {} event".format(host, sensorgroup_name, eventlevel)
+            else:
+                assert not events, "Event logged unexpectedly for sensor on {}".format(host)
+                system_helper.wait_for_alarm_gone(EventLogID.BMC_SENSOR_ACTION, entity_id=entity_id, strict=False,
+                                                  timeout=5, fail_ok=False)
+
+        LOG.tc_step("Check the host status for sensor: {}".format(sensor_name))
+        host_state_timeout = 120
+        if action == 'reset':
+            host_state_timeout = 1080  # 15 min reset interval in between two reset triggers
+        host_helper.wait_for_host_states(host, timeout=host_state_timeout, fail_ok=False,
+                                         availability=expected_host_state)
+        if action == 'power-cycle':
+            host_helper.wait_for_host_states(host, timeout=20, task=HostTask.POWER_CYCLE, strict=False)
+
+        LOG.tc_step("Check the alarm clears and host in available state after clearing events")
+        bmc_helper.clear_events(host)
+        system_helper.wait_for_alarm_gone(alarm_id=EventLogID.BMC_SENSOR_ACTION, entity_id=host, strict=False,
+                                          timeout=60)
+        wait_time = 3000 if action == 'power-cycle' else HostTimeout.REBOOT
+        host_helper.wait_for_host_states(host, fail_ok=False, timeout=wait_time, availability='available')
+
+    HOST = ''
+
+
 # @mark.usefixtures('bmc_test_prep')
 @mark.parametrize(('host', 'event_type', 'action_level', 'action', 'suppression', 'expt_alarm', 'expt_host_avail',
                    'new_action', 'new_suppression', 'new_expt_alarm', 'new_expt_host_avail'), [
