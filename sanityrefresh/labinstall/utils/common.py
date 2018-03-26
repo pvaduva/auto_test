ab607083082c40a27dd5eeea01d4eb42800046c9
Eric Barrett <eric.barrett@windriver.com>
2018-03-26 13:17:42 -0400

    Fixed auto installer defects: - wipedisk: no longer fails if it can't ssh controller-0 - empty arguments: added support to leave fields as '' - settings discovery: improved discovery of files when using custom configurations.
    Added functionality:
    node information: node information is now collected through VLM as
    well as the .ini files

--- sanityrefresh/labinstall/utils/common.py
+++ sanityrefresh/labinstall/utils/common.py
@@ -8,14 +8,13 @@ Copyright (c) 2015-2016 Wind River Systems, Inc.
 The right to copy, distribute, modify, or otherwise make use
 of this software may be licensed only pursuant to the terms
 of an applicable Wind River license agreement.
-    
-    
+
+
 modification history:
 ---------------------
 22feb16,amf  Using the full path for the node data file
 02dec15,kav  initial version
 '''
-
 
 from constants import *
 from .log import getLogger, print_step
@@ -33,30 +32,38 @@ from email.mime.text import MIMEText
 log = getLogger(__name__)
 
 
-#TODO: Would be nice to have a common function which can perform an
+# TODO: Would be nice to have a common function which can perform an
 #      action on a node (e.g. lock, unlock)
 #      It would take parameter host_name and action='unlock', say by default
 
-#TODO: Decide if functions should return 1 instead of sys.exit(1)
+# TODO: Decide if functions should return 1 instead of sys.exit(1)
 
 def create_node_dict(nodes, personality):
-    """Read .ini file for each node and create Host object for the node.
+    """Get attributes of each node through VLM and create Host object for
+    the node
 
-    The data in the .ini file is read into a dictionary which is used to
-    create a Host object for the node.
+    Read .ini file for necessary information not provided by VLM:
+    telnet_login_prompt, nic, and web console
+
+
+    The data is read into a dictionary which is used to create a Host object
+    for the node.
 
     Return dictionary of node names mapped to their respective Host objects.
     """
-    node_dict = {}
-    i = 0
-    
-    for node in nodes:
+    # Reserve the nodes so their telnet information can be collected
+    # Unreserve first to close any telnet sessions
+    vlm_unreserve(nodes)
+    vlm_reserve(nodes, note=INSTALLATION_RESERVE_NOTE)
 
-        config = configparser.ConfigParser()
+    nodes_info = vlm_getattr(nodes)
+    node_dict = {}
+    config = configparser.ConfigParser()
+    for i in range(0, len(nodes_info)):
         try:
             local_path = os.path.dirname(__file__)
             node_filepath = os.path.join(local_path, '..',
-                                         NODE_INFO_DIR, '%s.ini' % node)
+                                         NODE_INFO_DIR, '%s.ini' % nodes[i])
             node_file = open(node_filepath, 'r')
             config.read_file(node_file)
         except Exception:
@@ -74,9 +81,12 @@ def create_node_dict(nodes, personality):
         name = personality + "-{}".format(i)
         node_info_dict['name'] = name
         node_info_dict['personality'] = personality
-        node_info_dict['barcode'] = node
-        node_dict[name]=Host(**node_info_dict)
-        i += 1
+        node_info_dict['host_ip'] = nodes_info[i]['IP Address']
+        node_info_dict['telnet_ip'] = nodes_info[i]['Terminal Server']
+        node_info_dict['host_name'] = nodes_info[i]['Target Alias']
+        node_info_dict['telnet_port'] = str(2000 + int(nodes_info[i]['Terminal Server Port']))
+        node_info_dict['barcode'] = nodes_info[i]['Target ID']
+        node_dict[name] = Host(**node_info_dict)
 
     print_step(personality + " nodes:")
     for key, value in sorted(node_dict.items()):
@@ -89,12 +99,14 @@ def create_node_dict(nodes, personality):
 def remove_markers(str):
     return str.translate({ord(OPEN_MARKER): '', ord(CLOSE_MARKER): ''})
 
+
 def find_error_msg(str, msg="error"):
     if re.search(msg, str, re.IGNORECASE):
         found_error_msg = True
     else:
         found_error_msg = False
     return found_error_msg
+
 
 def pexpect_exec_cmd(cmd, tmout=10, event_dict=None):
     """Run cmd on localhost using pexpect.run.
@@ -107,7 +119,8 @@ def pexpect_exec_cmd(cmd, tmout=10, event_dict=None):
     log.info(cmd)
     (output, rc) = pexpect.run(cmd, timeout=tmout, withexitstatus=1,
                                events=event_dict)
-    return (rc, output.decode('utf-8','ignore'))
+    return (rc, output.decode('utf-8', 'ignore'))
+
 
 def exec_cmd(cmd, show_output=True):
     rc = 0
@@ -123,6 +136,7 @@ def exec_cmd(cmd, show_output=True):
     log.info("Return code: " + str(rc))
     return (rc, output)
 
+
 def get_ssh_key():
     ssh_key_fpath = os.path.expanduser(SSH_KEY_FPATH)
     if not os.path.isfile(ssh_key_fpath):
@@ -135,13 +149,14 @@ def get_ssh_key():
     ssh_key = exec_cmd(GET_PUBLIC_SSH_KEY_CMD.format(ssh_key_fpath).split())[1]
     return ssh_key
 
+
 def vlm_reserve(barcodes, note=None):
     total_barcodes = []
     # number of targets we can reserve in VLM at once
     n = 5
     if isinstance(barcodes, str):
         barcodes = [barcodes]
-    chunks = [barcodes[i:i+n] for i in range(0,len(barcodes), n)]
+    chunks = [barcodes[i:i + n] for i in range(0, len(barcodes), n)]
     action = VLM_RESERVE
     for chunk in chunks:
         print(chunk)
@@ -150,11 +165,11 @@ def vlm_reserve(barcodes, note=None):
         [cmd.append(barcode) for barcode in chunk]
         reserve_note_params = []
         if note is not None:
-            #reserve_note_params = ["-n", note]
+            # reserve_note_params = ["-n", note]
             reserve_note_params = ["-n", '"{}"'.format(note)]
             cmd += reserve_note_params
             print("This is cmd: %s" % cmd)
-            #reserved_barcodes = exec_cmd(cmd)
+            # reserved_barcodes = exec_cmd(cmd)
             reserved_barcodes = exec_cmd(cmd)[1]
             if not reserved_barcodes or "Error" in reserved_barcodes:
                 msg = "Failed to reserve target(s): " + str(chunk)
@@ -168,12 +183,14 @@ def vlm_reserve(barcodes, note=None):
         log.error(msg)
         wr_exit()._exit(1, msg)
 
+
 def vlm_unreserve(barcodes):
     log.info("Target(s) nodes: {}".format(str(barcodes)))
     reservedbyme = vlm_findmine()
     for bc in barcodes:
         if bc in reservedbyme:
             vlm_exec_cmd(VLM_UNRESERVE, bc)
+
 
 def vlm_getattr(barcodes):
     attr_values = []
@@ -191,7 +208,7 @@ def vlm_getattr(barcodes):
 
         for line in output.splitlines():
             if line:
-                attr, val = re.split("\s*:\s+", line)
+                attr, val = re.split("\s*:\s+", line)[0:2]
                 attr_dict[attr] = val
         attr_values.append(attr_dict)
     return attr_values
@@ -204,10 +221,11 @@ def vlm_findmine():
         reserved_targets = output.split()
         msg = "Target(s) reserved by user: {}".format(str(reserved_targets))
     else:
-       msg = "User has no reserved target(s)"
-       reserved_targets = []
+        msg = "User has no reserved target(s)"
+        reserved_targets = []
     log.info(msg)
     return reserved_targets
+
 
 def vlm_exec_cmd(action, barcode):
     if action not in VLM_CMDS_REQ_RESERVE:
@@ -229,13 +247,13 @@ def vlm_exec_cmd(action, barcode):
 
 
 class wr_exit(object):
-
     class _wr_exit():
         """Exit utility.
 
         Wr_Exit contains various attributes  such exit status, error messages and methods
         for  Email notification of lab install status before exit.
         """
+
         def __init__(self):
 
             self.status = 0
@@ -271,12 +289,12 @@ class wr_exit(object):
                 if os.path.exists(file_path):
                     os.remove(file_path)
 
-            #check if we need to send email
+            # check if we need to send email
             if self.email_to is not None:
                 _msg = ''
                 if status is not 0:
                     self.email_subject += " failed"
-                    _msg += self.lab_name + "\n" +  EMAIL_ERROR_MSG + msg
+                    _msg += self.lab_name + "\n" + EMAIL_ERROR_MSG + msg
                 else:
                     self.email_subject += " completed"
                     _msg += self.lab_name + "\n" + msg
@@ -321,7 +339,6 @@ class wr_exit(object):
 
     def __setattr__(self, name):
         return setattr(self.instance, name)
-
 
 
 class install_step(object):
