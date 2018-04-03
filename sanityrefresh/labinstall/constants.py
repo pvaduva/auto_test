83fc7450ca21177a2ee54ac6b4d9e60f55b8d9da
Maria Yousaf <maria.yousaf@windriver.com>
2018-04-03 16:03:10 -0400

    Relax match for HP BIOS type

--- sanityrefresh/labinstall/constants.py
+++ sanityrefresh/labinstall/constants.py
@@ -119,7 +119,7 @@ PATCH_AVAILABLE_STATE = "Available"
 DEFAULT_BOOT_DEVICE_DICT = {'controller-0': '[ABC]00',
                             'compute': '[ABC]01',
                             'storage': '[ABC]01'}
-BIOS_TYPES = [b"American Megatrends", b"Hewlett-Packard", b"Phoenix", b"PowerEdge", b"American Megatrends"]
+BIOS_TYPES = [b"American Megatrends", b"Hewlett", b"Phoenix", b"PowerEdge", b"American Megatrends"]
 # BIOS_TYPE_FN_KEY_ESC_CODES = ['\x1b' + '[17~', '\x1b' + '@', '\x1b' + '[24~', '\x1b' + '[23~', '\x1b!']
 BIOS_TYPE_FN_KEY_ESC_CODES = ['\x1b' + '[17~', '\x1b' + '@', '\x1b' + '[24~', '\x1b' + '[23~',  '\x1b' + '!']
 # F6, ESC + @, F12 Phoenix used for R720 nodes (i.e. Dell), F6 for supermicro
