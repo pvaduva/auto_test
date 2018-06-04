bbbb933bbb7ae07d0ac52e3b4dcbba35a8bb085e
Yang Liu <yang.liu@windriver.com>
2018-06-04 15:28:41 -0400

    adding a debug msg to show pip version, since it fails on test server.

--- CGCSAuto/utils/clients/local.py
+++ CGCSAuto/utils/clients/local.py
@@ -168,6 +168,8 @@ class LocalHostClient(SSHClient):
             new_prompt = '\({}\) {}'.format(venv_name, self.get_prompt())
             self.set_prompt(prompt=new_prompt)
             LOG.info('virtualenv {} activated successfully'.format(venv_name))
+        output = self.exec_cmd('pip -V')[1]
+        LOG.info("'pip -V' output: {}".format(output))
 
     def deactivate_virtualenv(self, venv_name, new_prompt=None):
         # determine on the new prompt
