89f9f0a6cd9956de7eff9353a7a3732100c6e5db
senthil <senthil.mukundakumar@windriver.com>
2018-06-13 13:17:51 -0500

    Change made to fix CGTS-9758. Added bring up interface after last interface attach.

--- CGCSAuto/testcases/functional/networking/test_interface_attach_detach.py
+++ CGCSAuto/testcases/functional/networking/test_interface_attach_detach.py
@@ -286,19 +286,27 @@ def test_interface_attach_detach_on_paused_vm(base_vm, guest_os, if_attach_arg, 
     LOG.tc_step("Detach all the {} interface {}".format(vif_model, tenant_port_ids))
     for tenant_port_id in tenant_port_ids:
         vm_helper.detach_interface(vm_id=vm_under_test, port_id=tenant_port_id)
-
-    vm_helper.attach_interface(vm_under_test, vif_model=vif_model, net_id=tenant_net_id)
-
-    for i in range(live_migrations):
-        vm_helper.perform_action_on_vm(vm_under_test, action='live_migrate')
-        vm_helper.wait_for_vm_pingable_from_natbox(vm_under_test)
-        _ping_vm_data(vm_under_test, base_vm_id, action='live migrate')
+        new_vnics -= 1
 
     LOG.tc_step("Remove the dhclient leases cache after detach")
     _remove_dhclient_cache(vm_id=vm_under_test)
     res = vm_helper.ping_vms_from_vm(to_vms=base_vm_id, from_vm=vm_under_test, fail_ok=True, retry=0,
                                      net_types=['data'])[0]
     assert not res, "Ping from base_vm to vm via detached interface still works"
+
+    LOG.tc_step("Attach single {} interface with tenant id {}".format(vif_model, tenant_net_id))
+    vm_helper.attach_interface(vm_under_test, vif_model=vif_model, net_id=tenant_net_id)
+    new_vnics += 1
+
+    LOG.tc_step("Perform following action(s) on vm  {}: {} {} time".format(vm_under_test, 'live migrate', i))
+    for i in range(live_migrations):
+        vm_helper.perform_action_on_vm(vm_under_test, action='live_migrate')
+
+    LOG.tc_step("Bring up all the attached new {} {} tenant interface from vm".format(new_vnics, vif_model))
+    _bring_up_attached_interface(vm_under_test, guest_os=guest_os, num=new_vnics + 1)
+    _ping_vm_data(vm_under_test, base_vm_id, action='live migrate')
+
+
 
 
 @mark.parametrize(('guest_os', 'if_attach_arg', 'boot_source'), [
