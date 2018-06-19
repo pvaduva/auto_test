0c16e859f4dbf6a9129bdea93ed92cb56e893d35
Eric Barrett <eric.barrett@windriver.com>
2018-06-19 13:28:16 -0400

    Fix of b7e9ce8a82f309d78f60d698660adb2a17ef1159

--- CGCSAuto/keywords/install_helper.py
+++ CGCSAuto/keywords/install_helper.py
@@ -2301,7 +2301,6 @@ def boot_controller(lab=None, bld_server_conn=None, patch_dir_paths=None, boot_u
         controller0.telnet_conn.expect(["assword:"])
         controller0.telnet_conn.send(HostLinuxCreds.get_password())
         host_name_split = controller0.host_name.split("-")
-        host_name_split.pop(-1)
         host_name_split[-1] = "\d+"
         host_name = '-'.join(host_name_split)
         host_name_regex = "(" + host_name + "|localhost" + ")"
