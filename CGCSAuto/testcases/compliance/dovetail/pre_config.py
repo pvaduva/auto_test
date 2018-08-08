a3b664f8218518b56ba50a7b6aa709f3e2954145
abaig <abdullah.baig@windriver.com>
2018-08-08 16:20:12 -0400

    Multi compute solution for obtaining IPs and generating tempest_conf.yaml
     On branch dovetail
     Your branch is up to date with 'origin/dovetail'.
     Changes to be committed:
    	modified:   CGCSAuto/testcases/compliance/dovetail/pre_config.py

--- CGCSAuto/testcases/compliance/dovetail/pre_config.py
+++ CGCSAuto/testcases/compliance/dovetail/pre_config.py
@@ -1,16 +1,15 @@
 import configparser
-import yaml
 import time
 import re
 from utils.tis_log import LOG
 from consts.auth import HostLinuxCreds, CliAuth, Tenant, ComplianceCreds
-from keywords import vlm_helper, host_helper, system_helper
+from keywords import vlm_helper, host_helper, system_helper, nova_helper, network_helper, cinder_helper
 from utils.clients.ssh import ControllerClient, SSHClient
 from utils import table_parser
 from consts.filepaths import Dovetail
 
 DOVETAIL_HOST = 'tis-dovetail-test-node.cumulus.wrs.com'
-
+TESTAREA_MANDATORY_MAX_TIMEOUT = 20000
 
 def env_config_generate(floating_ip, server_ssh):
     con_ssh = ControllerClient.get_active_controller()
@@ -26,13 +25,13 @@ def env_config_generate(floating_ip, server_ssh):
     config.optionxform = str
     filepath = '${DOVETAIL_HOME}/pre_config/env_config.sh'
 
-    server_ssh.exec_cmd('sed -i "s/^export OS_PROJECT_NAME=.*/export OS_PROJECT_NAME= {}/g" {}'.format(user_admin, filepath))
-    server_ssh.exec_cmd('sed -i "s/^export OS_TENANT_NAME=.*/export OS_TENANT_NAME= {}/g" {}'.format(user_admin, filepath))
-    server_ssh.exec_cmd('sed -i "s/^export OS_USERNAME=.*/export OS_USERNAME= {}/g" {}'.format(user_admin, filepath))
-    server_ssh.exec_cmd('sed -i "s/^export OS_PASSWORD=.*/export OS_PASSWORD= {}/g" {}'.format(password_admin, filepath))
-    server_ssh.exec_cmd('sed -i "s/^export OS_AUTH_URL=.*/export OS_AUTH_URL= {}/g" {}'.format(Dovetail.OS_AUTH_URL.format((floating_ip)), filepath))
-    server_ssh.exec_cmd('sed -i "s/^export OS_IDENTITY_API_VERSION=.*/export OS_IDENTITY_API_VERSION= {}/g" {}'.format(CliAuth.get_var('OS_IDENTITY_API_VERSION'), filepath))
-    server_ssh.exec_cmd('sed -i "s/^export OS_PROJECT_ID=.*/export OS_PROJECT_ID= {}/g" {}'.format(projectID_admin, filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_PROJECT_NAME=.*/export OS_PROJECT_NAME={}/g" {}'.format(user_admin, filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_TENANT_NAME=.*/export OS_TENANT_NAME={}/g" {}'.format(user_admin, filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_USERNAME=.*/export OS_USERNAME={}/g" {}'.format(user_admin, filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_PASSWORD=.*/export OS_PASSWORD={}/g" {}'.format(password_admin, filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_AUTH_URL=.*/export OS_AUTH_URL={}/g" {}'.format(Dovetail.OS_AUTH_URL.format((floating_ip)), filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_IDENTITY_API_VERSION=.*/export OS_IDENTITY_API_VERSION={}/g" {}'.format(CliAuth.get_var('OS_IDENTITY_API_VERSION'), filepath))
+    server_ssh.exec_cmd('sed -i "s/^export OS_PROJECT_ID=.*/export OS_PROJECT_ID={}/g" {}'.format(projectID_admin, filepath))
 
 
     # config[None] = {'# Project-level authentication scope (name or ID), recommend admin project.':'',
@@ -123,11 +122,11 @@ def pod_generate_2plus2(n1ip, n2ip, n3ip, n4ip, server_ssh):
 #     return config
 
 
-def tempest_conf_generate_2plus2(server_ssh):
+def tempest_conf_generate(computes, server_ssh):
 
-    server_ssh.exec_sudo_cmd('sed -i "s/^min_compute_nodes:.*/min_compute_nodes: 2/g" ${DOVETAIL_HOME}/pre_config/tempest_conf.yaml')
-    server_ssh.exec_sudo_cmd('sed -i "s/^volume_device_name:.*/volume_device_name: vdb/g" ${DOVETAIL_HOME}/pre_config/tempest_conf.yaml')
-
+    server_ssh.exec_sudo_cmd('sed -i "s/.*min_compute_nodes:.*/  min_compute_nodes: '+str(computes)+'/g" ${DOVETAIL_HOME}/pre_config/tempest_conf.yaml')
+    server_ssh.exec_sudo_cmd('sed -i "s/.*volume_device_name:.*/  volume_device_name: vdb/g" ${DOVETAIL_HOME}/pre_config/tempest_conf.yaml')
+    print(server_ssh.exec_cmd('cat ${DOVETAIL_HOME}/pre_config/tempest_conf.yaml'))
 #     sample = '''compute:
 #   # The minimum number of compute nodes expected.
 #
@@ -184,26 +183,52 @@ def test():
     floating_ip = lab_info.get('floating ip')
     # controller1_ip = lab_info.get('controller-1 ip')
 
-    LOG.info("Connecting to active Controller")
+    system_nodes = system_helper.get_hostnames()
+    storage_nodes = [h for h in system_nodes if "storage" in h]
+    compute_nodes = [h for h in system_nodes if "storage" not in h and 'controller' not in h]
+    compute_ips = []
+    storage_ips = []
+    print(compute_nodes)
 
+    LOG.info("Connecting to active Controller")
     con_ssh = ControllerClient.get_active_controller()
     con_ssh.connect()
-    compute0_ip = con_ssh.exec_cmd('nslookup compute-0')
-    compute0_ip = compute0_ip[1]
-    compute0_ip = compute0_ip.split('Address')
-    compute0_ip = compute0_ip[-1]
-    compute0_ip = compute0_ip[2:]
 
-    compute1_ip = con_ssh.exec_cmd('nslookup compute-1')
-    compute1_ip = compute1_ip[1]
-    compute1_ip = compute1_ip.split('Address')
-    compute1_ip = compute1_ip[-1]
-    compute1_ip = compute1_ip[2:]
+    for computes in compute_nodes:
+        ip = con_ssh.exec_cmd('nslookup '+computes)
+        ip = ip[1]
+        ip = ip.split('Address')
+        ip = ip[-1]
+        ip = ip[2:]
+        compute_ips.append(ip)
+    print(compute_ips)
+
+    for storage in storage_nodes:
+        ip = con_ssh.exec_cmd('nslookup '+storage)
+        ip = ip[1]
+        ip = ip.split('Address')
+        ip = ip[-1]
+        ip = ip[2:]
+        storage_ips.append(ip)
+
+    # compute0_ip = con_ssh.exec_cmd('nslookup compute-0')
+    # compute0_ip = compute0_ip[1]
+    # compute0_ip = compute0_ip.split('Address')
+    # compute0_ip = compute0_ip[-1]
+    # compute0_ip = compute0_ip[2:]
+    #
+    # compute1_ip = con_ssh.exec_cmd('nslookup compute-1')
+    # compute1_ip = compute1_ip[1]
+    # compute1_ip = compute1_ip.split('Address')
+    # compute1_ip = compute1_ip[-1]
+    # compute1_ip = compute1_ip[2:]
 
     LOG.info("Generating YAML files")
 
-    pod_generate_2plus2('192.168.204.3', '192.168.204.3', compute0_ip, compute1_ip, server_ssh)
-    tempest_conf_generate_2plus2(server_ssh)
+    if len(compute_ips) == 2:
+        pod_generate_2plus2('192.168.204.3', '192.168.204.4', compute_ips[0], compute_ips[1], server_ssh)
+
+    tempest_conf_generate(len(compute_ips), server_ssh)
     env_config_generate(floating_ip, server_ssh)
 
     password = HostLinuxCreds.get_password()
@@ -250,11 +275,17 @@ def test():
     filepath = filepath[-1]
     server_ssh.exec_sudo_cmd("sed -ie 's/processes=.*/processes="+stdout+"/g' "+filepath)
 
+    LOG.info("Updating Quotas")
+    nova_helper.update_quotas(tenant='admin', instances=20, cores=50)
+    cinder_helper.update_quotas(tenant='admin', volumes=100, snapshots=100)
+    network_helper.update_quotas(tenant_name='admin', port=500, floatingip=100, subnet=100, network=100)
+
+
     LOG.info("Sourcing config files")
     server_ssh.exec_cmd('export DOVETAIL_HOME='+Dovetail.DOVETAIL_HOME)
     server_ssh.exec_cmd('source ${DOVETAIL_HOME}/pre_config/env_config.sh')
 
     LOG.info("Starting Docker Container")
     server_ssh.exec_sudo_cmd("docker run --privileged=true -it -e DOVETAIL_HOME=$DOVETAIL_HOME -v $DOVETAIL_HOME:$DOVETAIL_HOME -v /var/run/docker.sock:/var/run/docker.sock opnfv/dovetail:ovp.1.0.0 /bin/bash")
-    LOG.info("Starting Dovetail Testsuite Mandatory")
-    server_ssh.exec_cmd('dovetail run --testsuite madatory')
+    LOG.info("Starting Dovetail Testarea Mandatory")
+    server_ssh.exec_cmd('dovetail run --testarea mandatory', expect_timeout=TESTAREA_MANDATORY_MAX_TIMEOUT)
