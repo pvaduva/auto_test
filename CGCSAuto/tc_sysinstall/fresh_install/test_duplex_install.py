ebb314593206d322da0f74e9f31a2dd523aa0225
Eric Barrett <eric.barrett@windriver.com>
2018-08-09 14:28:39 -0400

    Fix for duplex bug

--- CGCSAuto/tc_sysinstall/fresh_install/test_duplex_install.py
+++ CGCSAuto/tc_sysinstall/fresh_install/test_duplex_install.py
@@ -67,6 +67,7 @@ def test_duplex_install(install_setup):
     fresh_install_helper.boot_hosts(boot_device)
 
     fresh_install_helper.run_lab_setup(con_ssh=controller0_node.ssh_conn)
+    fresh_install_helper.run_lab_setup(con_ssh=controller0_node.ssh_conn)
 
     fresh_install_helper.unlock_hosts(["controller-1"], con_ssh=controller0_node.ssh_conn)
 
