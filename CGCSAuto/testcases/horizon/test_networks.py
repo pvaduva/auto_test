5a9715b7e40c87a5ee0534455eb917bbbe121b4a
mlei <ming.lei@windriver.com>
2018-09-26 10:48:00 -0400

    fix CGTS-10206 - hardcoded pnet name/type and project name.

--- CGCSAuto/testcases/horizon/test_networks.py
+++ CGCSAuto/testcases/horizon/test_networks.py
@@ -1,11 +1,13 @@
+from pytest import fixture, skip
+from utils.tis_log import LOG
 from utils.horizon.regions import messages
 from utils.horizon.pages.project.network import networkspage as pro_networkspage
 from utils.horizon.pages.admin.network import networkspage as adm_networkspage
 from utils.horizon import helper
-from pytest import fixture
-from testfixtures.horizon import tenant_home_pg, admin_home_pg, driver
-from utils.tis_log import LOG
+from keywords import network_helper
 from consts import horizon
+from consts import auth
+from testfixtures.horizon import tenant_home_pg, admin_home_pg, driver    #Do not remove
 
 
 @fixture(scope='function')
@@ -64,6 +66,16 @@ def test_network_subnet_create_tenant(project_networks_pg):
     horizon.test_result = True
 
 
+@fixture()
+def get_pnet():
+    pnet_name = 'group0-data0'
+    pnet_type = network_helper.get_providernets(rtn_val='type', name=pnet_name)
+    if not pnet_type:
+        skip('{} does not exist. skip test.'.format(pnet_name))
+
+    return pnet_name, pnet_type[0]
+
+
 @fixture(scope='function')
 def admin_networks_pg(admin_home_pg, request):
     LOG.fixture_step('Go to Admin > Network > Networks')
@@ -79,7 +91,7 @@ def admin_networks_pg(admin_home_pg, request):
     return networks_pg
 
 
-def test_network_subnet_create_admin(admin_networks_pg):
+def test_network_subnet_create_admin(get_pnet, admin_networks_pg):
     """
     Test the network creation and deletion functionality:
 
@@ -97,13 +109,15 @@ def test_network_subnet_create_admin(admin_networks_pg):
         - Delete the newly created network
         - Verify the network does not appear in the table after deletion
     """
+    pnet_name, pnet_type = get_pnet
     network_name = helper.gen_resource_name('network')
     subnet_name = helper.gen_resource_name('subnet')
+
     LOG.tc_step('Create new network {}.'.format(network_name))
     admin_networks_pg.create_network(network_name,
-                                     project='tenant1',
-                                     provider_network_type='vlan',
-                                     physical_network='group0-data0',
+                                     project=auth.Tenant.TENANT1['tenant'],
+                                     provider_network_type=pnet_type,
+                                     physical_network=pnet_name,
                                      subnet_name=subnet_name,
                                      network_address='192.168.0.0/24')
     assert admin_networks_pg.find_message_and_dismiss(messages.SUCCESS)
