32d6dfcf4a8167e61b66c475c218c6e95231234c
mlei <ming.lei@windriver.com>
2018-11-21 16:32:05 -0500

    Add test_storage_overview.py and its helper pages

---
+++ CGCSAuto/testcases/horizon/test_storage_overview.py
@@ -0,0 +1,129 @@
+from pytest import fixture, skip, mark
+
+from utils import table_parser, cli
+from utils.tis_log import LOG
+from utils.clients.ssh import ControllerClient
+from utils.horizon.pages.admin.platform import storageoverviewpage
+from consts import horizon
+from keywords import system_helper, storage_helper
+from testfixtures.horizon import admin_home_pg
+
+
+@fixture()
+def storage_precheck():
+    if not system_helper.is_storage_system():
+        skip('This test only applies to storage nodes')
+
+
+@fixture()
+def storage_overview_pg(admin_home_pg):
+    LOG.fixture_step('Go to Admin > Platform > Storage Overview')
+    storage_overview_pg = storageoverviewpage.StorageOverviewPage(admin_home_pg.driver)
+    storage_overview_pg.go_to_target_page()
+
+    return storage_overview_pg
+
+
+@mark.usefixtures('storage_precheck')
+def test_horizon_storage_overview_service_display(storage_overview_pg):
+    """
+    Tests the storage overview display:
+
+    Setups:
+        - Login as Admin
+        - Go to Admin > Platform > Storage Overview
+
+    Teardown:
+        - Logout
+
+    Test Steps:
+        - Test Storage cluster UUID, Health Status and Details display
+        - Test host and rank table display
+        - Test osd.# table and status display
+    """
+    storage_overview_pg.go_to_services_tab()
+    con_ssh = ControllerClient.get_active_controller()
+    LOG.tc_step('Check storage cluster UUID, ceph health and storage usage display')
+    cli_ceph_info = []
+
+    uuid = system_helper.get_cluster_values(header='cluster_uuid')[0]
+    cli_ceph_info.append(uuid)
+
+    health_status = con_ssh.exec_cmd('ceph health')[1]
+    cli_ceph_info.append(health_status)
+
+    health_details = con_ssh.exec_cmd('ceph health detail')[1]
+    cli_ceph_info.append(health_details)
+
+    horizon_ceph_info = storage_overview_pg.get_horizon_ceph_info_dict()
+    for info in cli_ceph_info:
+        assert info in horizon_ceph_info.values(), 'Horizon storage cluster info does not match to cli info'
+    LOG.tc_step('Storage service details display correct')
+
+    LOG.info('Test host and rank table display')
+    ceph_mon_status = eval(con_ssh.exec_cmd('ceph mon_status')[1])
+    mon_map = ceph_mon_status.get('monmap')
+    cli_ceph_monitor = {}
+    # mon_map.get('mons') returns a dict list
+    for mon_info_dict in mon_map.get('mons'):
+        host_name = mon_info_dict.get('name')
+        host_rank = mon_info_dict.get('rank')
+        cli_ceph_monitor[host_name] = str(host_rank)
+
+    for host_name in cli_ceph_monitor.keys():
+        cli_rank_val = cli_ceph_monitor[host_name]
+        horizon_rank_val = storage_overview_pg.get_storage_overview_monitor_info(host_name, 'Rank')
+        assert horizon_rank_val == cli_rank_val, '{} rank display incorrectly'.format(host_name)
+
+    LOG.info('Host and rank table table display correct')
+
+    LOG.tc_step('Test osd table and status display')
+    osd_list = storage_helper.get_osds()
+    for osd_id in osd_list:
+        expt_horizon = {}
+        for header in storage_overview_pg.osds_table.column_names:
+            host_name = storage_helper.get_osd_host(osd_id)[0]
+            osd_name = 'osd.{}'.format(osd_id)
+            expt_horizon['Host'] = host_name
+            expt_horizon['Name'] = osd_name
+            expt_horizon['Status'] = 'up'
+            if not storage_helper.is_osd_up(osd_id, con_ssh):
+                expt_horizon['Status'] = 'down'
+            horizon_val = storage_overview_pg.get_storage_overview_osd_info(osd_name, header)
+            assert expt_horizon[header] == horizon_val, '{}{} display incorrectly'.format(osd_name, header)
+    LOG.info('Osd table display correct')
+    horizon.test_result = True
+
+
+def test_horizon_storage_overview_usage_display(storage_overview_pg):
+    """
+    Tests the storage overview display:
+
+    Setups:
+        - Login as Admin
+        - Go to Admin > Platform > Storage Overview
+
+    Teardown:
+        - Logout
+
+    Test Steps:
+        - Test backend type and name, service name, total capacity and free capacity(GiB) display
+    """
+    LOG.tc_step('Test usage details display')
+    storage_overview_pg.go_to_usage_tab()
+    usage_table = table_parser.table(cli.system('storage-usage-list --nowrap'))
+    horizon_headers = ['Backend type', 'Backend name', 'Service name', 'Free Capacity (GiB)', 'Total Capacity (GiB)']
+    rows = storage_overview_pg.get_rows_from_usage_table()
+    horizon_rows_val = []
+    for row in rows:
+        row_dict = []
+        for header in horizon_headers:
+            row_dict.append(row.cells[header].text)
+        horizon_rows_val.append(row_dict)
+    cli_rows_val = usage_table.get('values')
+
+    for row in cli_rows_val:
+        assert row in horizon_rows_val, 'row {} not found in usage table.'.format(row)
+
+    LOG.info('Storage overview usage details display correct')
+    horizon.test_result = True
--- CGCSAuto/utils/horizon/pages/admin/platform/storageoverviewpage.py
+++ CGCSAuto/utils/horizon/pages/admin/platform/storageoverviewpage.py
@@ -1,20 +1,102 @@
 from utils.horizon.pages import basepage
-from utils.horizon.regions import tables
+from utils.horizon.regions import tables, fluidinfo
+
+
+class HealthFluidInfoDetail(fluidinfo.FluidInfo):
+    name = "health"
+
+
+class StorageFluidInfoDetail(fluidinfo.FluidInfo):
+    name = "storage"
+
+
+class MonitorsTable(tables.TableRegion):
+    name = "monitors"
+
+
+class OsdsTable(tables.TableRegion):
+    name = "osds"
 
 
 class UsageTable(tables.TableRegion):
     name = "usage"
-    pass
 
 
 class StorageOverviewPage(basepage.BasePage):
-
     PARTIAL_URL = 'admin/storage_overview'
     SERVICES_TAB_INDEX = 0
+    SERVICES_MONITOR_HOST_COL = 'Host'
+    SERVICES_OSD_NAME_COL = 'Name'
     USAGE_TAB_INDEX = 1
+    USAGE_BACKEND_TYPE_COL = 'Backend type'
+    USAGE_BACKEND_SERVICE_NAME_COLUMN = 'Service name'
+
+    usage_headers_map = {
+        'Backend type': 'backend type',
+        'Backend name': 'backend name',
+        'Service name': 'service',
+        'Total Capacity (GiB)': 'total capacity (GiB)',
+        'Free Capacity (GiB)': 'free capacity (GiB)'
+    }
+
+    @property
+    def health_info(self):
+        return HealthFluidInfoDetail(self.driver)
+
+    @property
+    def storage_info(self):
+        return StorageFluidInfoDetail(self.driver)
+
+    @property
+    def monitors_table(self):
+        return MonitorsTable(self.driver)
+
+    @property
+    def osds_table(self):
+        return OsdsTable(self.driver)
+
+    @property
+    def usage_table(self):
+        return UsageTable(self.driver)
+
+    def _get_row_with_service_name(self, name):
+        return self.usage_table.get_row(self.USAGE_BACKEND_SERVICE_NAME_COLUMN, name)
+
+    def get_rows_from_usage_table(self):
+        return self.usage_table.rows
+
+    def _get_monitor_table_row_with_host(self, host_name):
+        return self.monitors_table.get_row(self.SERVICES_MONITOR_HOST_COL, host_name)
+
+    def _get_osd_table_row_with_osd_name(self, osd_name):
+        return self.osds_table.get_row(self. SERVICES_OSD_NAME_COL, osd_name)
+
+    def get_storage_overview_monitor_info(self, host_name, header):
+        row = self._get_monitor_table_row_with_host(host_name)
+        return row.cells[header].text
+
+    def get_storage_overview_osd_info(self, name, header):
+        row = self._get_osd_table_row_with_osd_name(name)
+        return row.cells[header].text
+
+    def get_storage_overview_usage_info(self, name, header):
+        row = self._get_row_with_service_name(name)
+        return row.cells[header].text
 
     def go_to_services_tab(self):
         self.go_to_tab(self.SERVICES_TAB_INDEX)
 
     def go_to_usage_tab(self):
         self.go_to_tab(self.USAGE_TAB_INDEX)
+
+    def get_horizon_ceph_info_dict(self):
+        health_headers = self.health_info.header_list
+        storage_info_headers = self.storage_info.header_list
+        health_vals = self.health_info.value_list
+        storage_info_vals = self.storage_info.value_list
+
+        header_list = health_headers + storage_info_headers
+        detail_vals = health_vals + storage_info_vals
+        ceph_info = dict(zip(header_list, detail_vals))
+
+        return ceph_info
