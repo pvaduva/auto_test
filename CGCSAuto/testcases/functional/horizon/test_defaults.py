dcfce4bc9b586a9875aa619afcedafc256afeb75
Ankit Jain <ankit.jain@windriver.com>
2019-03-14 11:30:56 -0400

    changed defaults test for tabbed page layout

--- CGCSAuto/testcases/functional/horizon/test_defaults.py
+++ CGCSAuto/testcases/functional/horizon/test_defaults.py
@@ -11,6 +11,7 @@ from utils.tis_log import LOG
 
 @fixture(scope='function')
 def defaults_pg(admin_home_pg_container, request):
+
     LOG.fixture_step('Go to Admin > System > Defaults')
     default_pg = defaultspage.DefaultsPage(admin_home_pg_container.driver, port=admin_home_pg_container.port)
     default_pg.go_to_target_page()
@@ -23,8 +24,9 @@ def defaults_pg(admin_home_pg_container, request):
     return default_pg
 
 
-def test_horizon_update_defaults(defaults_pg):
-    """Tests the Update Default Quotas functionality:
+def test_horizon_update_compute_defaults(defaults_pg):
+    """
+    Tests the Update Compute Default Quotas functionality:
 
     Setups:
         - Login as Admin
@@ -35,27 +37,53 @@ def test_horizon_update_defaults(defaults_pg):
         - Logout
 
     Test Steps:
-        - Updates default Quotas by adding a random number between 1 and 10
+        - Go to Compute Quotas Tab
+        - Updates default Compute Quotas by adding a random number between 1 and 10
         - Verifies that the updated values are present in the
-           Quota Defaults table
-        - Updates default Quotas back to original status
+           Quota Compute Defaults table
+        - Updates default Compute Quotas back to original status
+        - Go to Volume Quotas Tab
+        - Updates default Volume Quotas by adding a random number between 1 and 10
+        - Verifies that the updated values are present in the
+           Quota Volume Defaults table
+        - Updates default Volume Quotas back to original status
     """
 
     add_up = random.randint(1, 10)
-    default_quota_values = defaults_pg.quota_values
+    defaults_pg.go_to_compute_quotas_tab()
+    default_compute_quota_values = defaults_pg.compute_quota_values
 
-    LOG.tc_step('Updates default Quotas by add {}.'.format(add_up))
-    defaults_pg.update_defaults(add_up)
+    LOG.tc_step('Updates default Compute Quotas by add {}.'.format(add_up))
+    defaults_pg.update_compute_defaults(add_up)
     assert defaults_pg.find_message_and_dismiss(messages.SUCCESS)
     assert not defaults_pg.find_message_and_dismiss(messages.ERROR)
 
-    LOG.tc_step('Verifies that the updated values are present in the Quota Defaults table')
-    assert len(default_quota_values) > 0
-    for quota_name in default_quota_values:
-        assert defaults_pg.is_quota_a_match(quota_name,
-                                            default_quota_values[quota_name]
+    LOG.tc_step('Verifies that the updated values are present in the Quota Default Computes table')
+    assert len(default_compute_quota_values) > 0
+    for quota_name in default_compute_quota_values:
+        assert defaults_pg.is_compute_quota_a_match(quota_name,
+                                            default_compute_quota_values[quota_name]
                                             + add_up)
-    LOG.tc_step('Updates default Quotas back to original status')
+    LOG.tc_step('Updates default Compute Quotas back to original status')
     time.sleep(1)
-    defaults_pg.update_defaults(-add_up)
+    defaults_pg.update_compute_defaults(-add_up)
+
+    defaults_pg.go_to_volume_quotas_tab()
+    default_volume_quota_values = defaults_pg.volume_quota_values
+
+    LOG.tc_step('Updates default Volume Quotas by add {}.'.format(add_up))
+    defaults_pg.update_volume_defaults(add_up)
+    assert defaults_pg.find_message_and_dismiss(messages.SUCCESS)
+    assert not defaults_pg.find_message_and_dismiss(messages.ERROR)
+
+    LOG.tc_step('Verifies that the updated values are present in the Quota Default Volumes table')
+    assert len(default_volume_quota_values) > 0
+    for quota_name in default_volume_quota_values:
+        assert defaults_pg.is_volume_quota_a_match(quota_name,
+                                                   default_volume_quota_values[quota_name]
+                                                   + add_up)
+    LOG.tc_step('Updates default Volume Quotas back to original status')
+    time.sleep(1)
+    defaults_pg.update_volume_defaults(-add_up)
     horizon.test_result = True
+
--- CGCSAuto/utils/horizon/pages/admin/system/defaultspage.py
+++ CGCSAuto/utils/horizon/pages/admin/system/defaultspage.py
@@ -3,97 +3,156 @@ from utils.horizon.regions import forms
 from utils.horizon.regions import tables
 
 
-class DefaultQuotasTable(tables.TableRegion):
-    name = "quotas"
+class DefaultComputeQuotasTable(tables.TableRegion):
+    name = "compute_quotas"
 
     UPDATE_DEFAULTS_FORM_FIELDS = (
-        "injected_file_content_bytes",
-        "metadata_items",
-        "server_group_members",
-        "server_groups",
-        "ram",
-        "key_pairs",
-        "injected_file_path_bytes",
-        "instances",
-        "injected_files",
+        ("instances",
         "cores",
+        "ram",
+        "metadata_items",
+        "key_pairs",
+        "server_groups",
+        "server_group_members",
+        "injected_files",
+        "injected_file_content_bytes",
+        "injected_file_path_bytes"),
+        ("volumes",
         "gigabytes",
-        "snapshots",
-        "volumes"
+        "snapshots")
     )
 
-    @tables.bind_table_action('update_defaults')
+    @tables.bind_table_action('update_compute_defaults')
     def update(self, update_button):
         update_button.click()
         self.wait_till_spinner_disappears()
-        return forms.FormRegion(
+        return forms.TabbedFormRegion(
             self.driver,
-            None,
-            field_mappings=self.UPDATE_DEFAULTS_FORM_FIELDS
+            self.UPDATE_DEFAULTS_FORM_FIELDS
         )
+
+
+class DefaultVolumeQuotasTable(DefaultComputeQuotasTable):
+    name = "volume_quotas"
+
+    @tables.bind_table_action('update_volume_defaults')
+    def update(self, update_button):
+        update_button.click()
+        self.wait_till_spinner_disappears()
+        return forms.TabbedFormRegion(
+            self.driver,
+            self.UPDATE_DEFAULTS_FORM_FIELDS
+        )
+
+
+class DefaultNetworkQuotasTable(tables.TableRegion):
+    name = "network_quotas"
 
 
 class DefaultsPage(basepage.BasePage):
     PARTIAL_URL = 'admin/defaults'
+    COMPUTE_QUOTAS_TAB = 0
+    VOLUME_QUOTAS_TAB = 1
+    NETWORK_QUOTAS_TAB = 2
     QUOTAS_TABLE_NAME_COLUMN = 'Quota Name'
     QUOTAS_TABLE_LIMIT_COLUMN = 'Limit'
-    DEFAULT_QUOTA_NAMES = [
-        'Injected File Content Bytes',
-        'Metadata Items',
-        'Server Group Members',
-        'Server Groups',
-        'RAM (MB)',
-        'Key Pairs',
+    DEFAULT_COMPUTE_QUOTA_NAMES = [
         'Instances',
-        'Injected Files',
         'VCPUs',
-        'Length of Injected File Path',
+        'RAM',
+        'Metadata Items',
+        'Key Pairs',
+        'Server Groups',
+        'Server Group Members',
+        'Injected Files',
+        'Injected File Content Bytes',
+        'Length of Injected File Path'
+    ]
+    DEFAULT_VOLUME_QUOTA_NAMES = [
+        'Volumes',
         'Total Size of Volumes and Snapshots (GiB)',
-        'Volume Snapshots',
-        'Volumes'
+        'Volume Snapshots'
     ]
 
-    def _get_quota_row(self, name):
-        return self.default_quotas_table.get_row(
+    def _get_compute_quota_row(self, name):
+        return self.default_compute_quotas_table.get_row(
+            self.QUOTAS_TABLE_NAME_COLUMN, name)
+
+    def _get_volume_quota_row(self, name):
+        return self.default_volume_quotas_table.get_row(
             self.QUOTAS_TABLE_NAME_COLUMN, name)
 
     @property
-    def default_quotas_table(self):
-        return DefaultQuotasTable(self.driver)
+    def default_compute_quotas_table(self):
+        return DefaultComputeQuotasTable(self.driver)
 
     @property
-    def quota_values(self):
+    def default_volume_quotas_table(self):
+        return DefaultVolumeQuotasTable(self.driver)
+
+    @property
+    def default_network_quotas_table(self):
+        return DefaultNetworkQuotasTable(self.driver)
+
+    @property
+    def compute_quota_values(self):
         quota_dict = {}
-        for row in self.default_quotas_table.rows:
+        for row in self.default_compute_quotas_table.rows:
             if row.cells[self.QUOTAS_TABLE_NAME_COLUMN].text in \
-                    self.DEFAULT_QUOTA_NAMES:
-                quota_dict[row.cells[self.QUOTAS_TABLE_NAME_COLUMN].text] =\
+                    self.DEFAULT_COMPUTE_QUOTA_NAMES:
+                quota_dict[row.cells[self.QUOTAS_TABLE_NAME_COLUMN].text] = \
                     int(row.cells[self.QUOTAS_TABLE_LIMIT_COLUMN].text)
         return quota_dict
 
-    def update_defaults(self, add_up):
-        update_form = self.default_quotas_table.update()
-        update_form.injected_file_content_bytes.value = \
-            int(update_form.injected_file_content_bytes.value) + add_up
+    @property
+    def volume_quota_values(self):
+        quota_dict = {}
+        for row in self.default_volume_quotas_table.rows:
+            if row.cells[self.QUOTAS_TABLE_NAME_COLUMN].text in \
+                    self.DEFAULT_VOLUME_QUOTA_NAMES:
+                quota_dict[row.cells[self.QUOTAS_TABLE_NAME_COLUMN].text] = \
+                    int(row.cells[self.QUOTAS_TABLE_LIMIT_COLUMN].text)
+        return quota_dict
 
+    def update_compute_defaults(self, add_up):
+        update_form = self.default_compute_quotas_table.update()
+        update_form.instances.value = int(update_form.instances.value) + add_up
+        update_form.cores.value = int(update_form.cores.value) + add_up
+        update_form.ram.value = int(update_form.ram.value) + add_up
         update_form.metadata_items.value = \
             int(update_form.metadata_items.value) + add_up
-        update_form.server_group_members.value = int(update_form.server_group_members.value) + add_up
-        update_form.server_groups.value = int(update_form.server_groups.value) + add_up
-        update_form.ram.value = int(update_form.ram.value) + add_up
         update_form.key_pairs.value = int(update_form.key_pairs.value) + add_up
-        update_form.injected_file_path_bytes.value = \
-            int(update_form.injected_file_path_bytes.value) + add_up
-        update_form.instances.value = int(update_form.instances.value) + add_up
+        update_form.server_groups.value = int(update_form.server_groups.value) + add_up
+        update_form.server_group_members.value = int(update_form.server_group_members.value) + add_up
         update_form.injected_files.value = int(
             update_form.injected_files.value) + add_up
-        update_form.cores.value = int(update_form.cores.value) + add_up
-        update_form.gigabytes.value = int(update_form.gigabytes.value) + add_up
-        update_form.snapshots.value = int(update_form.snapshots.value) + add_up
-        update_form.volumes.value = int(update_form.volumes.value) + add_up
-
+        update_form.injected_file_content_bytes.value = \
+            int(update_form.injected_file_content_bytes.value) + add_up
+        update_form.injected_file_path_bytes.value = \
+            int(update_form.injected_file_path_bytes.value) + add_up
         update_form.submit()
 
-    def is_quota_a_match(self, quota_name, limit):
-        row = self._get_quota_row(quota_name)
+    def update_volume_defaults(self, add_up):
+        update_form = self.default_volume_quotas_table.update()
+        update_form.switch_to(self.VOLUME_QUOTAS_TAB)
+        update_form.volumes.value = int(update_form.volumes.value) + add_up
+        update_form.gigabytes.value = int(update_form.gigabytes.value) + add_up
+        update_form.snapshots.value = int(update_form.snapshots.value) + add_up
+        update_form.submit()
+
+    def is_compute_quota_a_match(self, quota_name, limit):
+        row = self._get_compute_quota_row(quota_name)
         return row.cells[self.QUOTAS_TABLE_LIMIT_COLUMN].text == str(limit)
+
+    def is_volume_quota_a_match(self, quota_name, limit):
+        row = self._get_volume_quota_row(quota_name)
+        return row.cells[self.QUOTAS_TABLE_LIMIT_COLUMN].text == str(limit)
+
+    def go_to_compute_quotas_tab(self):
+        self.go_to_tab(self.COMPUTE_QUOTAS_TAB)
+
+    def go_to_volume_quotas_tab(self):
+        self.go_to_tab(self.VOLUME_QUOTAS_TAB)
+
+    def go_to_network_quotas_tab(self):
+        self.go_to_tab(self.NETWORK_QUOTAS_TAB)
