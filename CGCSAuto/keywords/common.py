8c0034082c2f490e071bf324866182e533e7797e
Yang Liu <yang.liu@windriver.com>
2019-06-13 22:20:18 -0400

    fix a bug in scp guest image

--- CGCSAuto/keywords/common.py
+++ CGCSAuto/keywords/common.py
@@ -84,24 +84,24 @@ def _scp_from_remote_server_to_active_controller(source_server, source_path, des
     nat_name = ProjVar.get_var('NATBOX')
     if nat_name:
         nat_name = nat_name.get('name')
-        if nat_name == 'localhost' or nat_name.startswith('128.224.'):
-            LOG.info('VBox detected, performing intermediate scp')
+    if nat_name and (nat_name == 'localhost' or nat_name.startswith('128.224.')):
+        LOG.info('VBox detected, performing intermediate scp')
 
-            nat_dest_path = '/tmp/{}'.format(dest_name)
-            nat_ssh = NATBoxClient.get_natbox_client()
+        nat_dest_path = '/tmp/{}'.format(dest_name)
+        nat_ssh = NATBoxClient.get_natbox_client()
 
-            if not nat_ssh.file_exists(nat_dest_path):
-                LOG.info("scp file from {} to NatBox: {}".format(nat_name, source_server))
-                nat_ssh.scp_on_dest(source_user=source_user, source_ip=source_server, source_path=source_path,
-                                    dest_path=nat_dest_path, source_pswd=source_password, timeout=timeout,
-                                    is_dir=is_dir)
+        if not nat_ssh.file_exists(nat_dest_path):
+            LOG.info("scp file from {} to NatBox: {}".format(nat_name, source_server))
+            nat_ssh.scp_on_dest(source_user=source_user, source_ip=source_server, source_path=source_path,
+                                dest_path=nat_dest_path, source_pswd=source_password, timeout=timeout,
+                                is_dir=is_dir)
 
-            LOG.info('scp file from natbox {} to active controller'.format(nat_name))
-            dest_user = HostLinuxCreds.get_user()
-            dest_pswd = HostLinuxCreds.get_password()
-            dest_ip = ProjVar.get_var('LAB').get('floating ip')
-            nat_ssh.scp_on_source(source_path=nat_dest_path, dest_user=dest_user, dest_ip=dest_ip, dest_path=dest_path,
-                                  dest_password=dest_pswd, timeout=timeout, is_dir=is_dir)
+        LOG.info('scp file from natbox {} to active controller'.format(nat_name))
+        dest_user = HostLinuxCreds.get_user()
+        dest_pswd = HostLinuxCreds.get_password()
+        dest_ip = ProjVar.get_var('LAB').get('floating ip')
+        nat_ssh.scp_on_source(source_path=nat_dest_path, dest_user=dest_user, dest_ip=dest_ip, dest_path=dest_path,
+                              dest_password=dest_pswd, timeout=timeout, is_dir=is_dir)
 
     else:   # if not a VBox lab, scp from remote server directly to TiS server
         LOG.info("scp file(s) from {} to tis".format(source_server))
