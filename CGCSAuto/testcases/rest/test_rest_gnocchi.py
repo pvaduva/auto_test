ebae6a747fc37dd193d04c4c4f6eae8b8384c1a5
Yang Liu <yang.liu@windriver.com>
2019-09-14 20:17:54 -0400

    skip gnocchi restAPI tests if no openstack applied

--- CGCSAuto/testcases/rest/test_rest_gnocchi.py
+++ CGCSAuto/testcases/rest/test_rest_gnocchi.py
@@ -6,7 +6,7 @@ from testcases.rest import rest_test_helper
 
 
 @pytest.fixture(scope='module')
-def gnocchi_rest():
+def gnocchi_rest(stx_openstack_required):
     r = Rest('gnocchi', platform=False)
     return r
 
@@ -21,4 +21,3 @@ def test_rest_gnocchi(gnocchi_rest, operation, resource):
     if operation == "GET":
         LOG.info("getting... {}".format(resource))
         rest_test_helper.get(gnocchi_rest, resource=resource)
-
