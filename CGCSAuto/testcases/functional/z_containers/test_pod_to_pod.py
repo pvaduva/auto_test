1c15b16dd451dcd35bd1d9d0525a46d065a5a448
Ayyappa Mantri <amantri@yow-amantri-lx.wrs.com>
2020-01-30 17:23:00 -0500

    added code to handle format for ipv6 cases

--- CGCSAuto/testcases/functional/z_containers/test_pod_to_pod.py
+++ CGCSAuto/testcases/functional/z_containers/test_pod_to_pod.py
@@ -154,7 +154,7 @@ def deploy_test_pods(request):
     return server_ips, [client_pod1_name, client_pod2_name]
 
 
-@mark.networking
+@mark.networking1
 class TestPodtoPod:
     def test_pod_to_pod_ping(self, deploy_test_pods):
         """
